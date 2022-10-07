c707018652413ca441282ead92d905d7a740a8b7
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2022-10-07 12:34:57 +0100

    GUA-426: Parse event in handler function
    This refactor means the `writeEvent` function only has to deal with
    native types and we can deal with any potential parse errors or data
    validation further up in the stack which will make this function
    smaller.

--- lambda/write-user-services/index.ts
+++ lambda/write-user-services/index.ts
@@ -1,9 +1,14 @@
 import { SQSEvent, SQSRecord } from "aws-lambda";
 
-export const writeEvent = async (record: SQSRecord): Promise<void> => {
-  console.log(record.body);
+import { UserServices } from "./models";
+
+export const writeEvent = (userServices: UserServices): void => {
+  console.log(userServices);
 };
 
 export const lambdaHandler = async (event: SQSEvent): Promise<void> => {
-  event.Records.map((record) => writeEvent(record));
+  for (let i = 0; i < event.Records.length; i++) {
+    const userServices: UserServices = JSON.parse(event.Records[i].body);
+    writeEvent(userServices);
+  }
 };
--- lambda/write-user-services/tests/index.test.ts
+++ lambda/write-user-services/tests/index.test.ts
@@ -1,26 +1,20 @@
 import { writeEvent } from "../index";
-import { SQSRecord } from "aws-lambda";
+import { UserServices } from "../models";
 
-const TEST_RECORD: SQSRecord = {
-  messageId: "19dd0b57-b21e-4ac1-bd88-01bbb068cb78",
-  receiptHandle: "MessageReceiptHandle",
-  body: "Hello from SQS!",
-  attributes: {
-    ApproximateReceiveCount: "1",
-    SentTimestamp: "1523232000000",
-    SenderId: "123456789012",
-    ApproximateFirstReceiveTimestamp: "1523232000001",
-  },
-  messageAttributes: {},
-  md5OfBody: "7b270e59b47ff90a553787216d55d91d",
-  eventSource: "aws:sqs",
-  eventSourceARN: "arn:aws:sqs:us-east-1:123456789012:MyQueue",
-  awsRegion: "us-east-1",
+const TEST_USER_SERVICES: UserServices = {
+  user_id: "user-id",
+  services: [
+    {
+      client_id: "client_id",
+      last_accessed: new Date(),
+      count_successful_logins: 1,
+    },
+  ],
 };
 
-test("it logs to the console", async () => {
+test("it logs to the console", () => {
   const consoleLogMock = jest.spyOn(console, "log").mockImplementation();
-  await writeEvent(TEST_RECORD);
+  writeEvent(TEST_USER_SERVICES);
   expect(consoleLogMock).toHaveBeenCalledTimes(1);
   consoleLogMock.mockRestore();
 });

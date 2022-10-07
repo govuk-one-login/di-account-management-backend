2165e5a5f2bd13719f96c4bb7563d99811bd8d7c
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2022-10-07 17:10:50 +0100

    GUA-426: Write parsed events to DynamoDB
    This commit sets up the 'happy path' for writing the incoming events to
    DynamoDB.
    
    The DynamoDB client is set up outside the handler function - this allows
    it to be reused if the function is called from a warm start, gaining us
    a bit of performance.
    
    The `userServices` object is logged to the console at the moment; this
    will need to be removed before we deploy to production to prevent
    leaking potentially sensitive data.

--- lambda/write-user-services/index.ts
+++ lambda/write-user-services/index.ts
@@ -1,14 +1,41 @@
-import { SQSEvent, SQSRecord } from "aws-lambda";
-
+import { SQSEvent } from "aws-lambda";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  PutCommand,
+  PutCommandOutput,
+} from "@aws-sdk/lib-dynamodb";
 import { UserServices } from "./models";
 
-export const writeEvent = (userServices: UserServices): void => {
+const TABLE_NAME = "process.env.TABLE_NAME";
+const marshallOptions = {
+  convertClassInstanceToMap: true,
+};
+const translateConfig = { marshallOptions };
+
+const dynamoClient = new DynamoDBClient({});
+const dynamoDocClient = DynamoDBDocumentClient.from(
+  dynamoClient,
+  translateConfig
+);
+
+export const writeEvent = async (
+  userServices: UserServices
+): Promise<PutCommandOutput> => {
+  const command = new PutCommand({
+    TableName: TABLE_NAME,
+    Item: {
+      user_id: userServices.user_id,
+      services: userServices.services,
+    },
+  });
   console.log(userServices);
+  return await dynamoDocClient.send(command);
 };
 
 export const lambdaHandler = async (event: SQSEvent): Promise<void> => {
   for (let i = 0; i < event.Records.length; i++) {
     const userServices: UserServices = JSON.parse(event.Records[i].body);
-    writeEvent(userServices);
+    await writeEvent(userServices);
   }
 };
--- lambda/write-user-services/tests/index.test.ts
+++ lambda/write-user-services/tests/index.test.ts
@@ -1,5 +1,9 @@
-import { writeEvent } from "../index";
+import { lambdaHandler, writeEvent } from "../index";
 import { UserServices } from "../models";
+
+import { mockClient } from "aws-sdk-client-mock";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { SQSEvent, SQSRecord } from "aws-lambda";
 
 const TEST_USER_SERVICES: UserServices = {
   user_id: "user-id",
@@ -12,9 +16,70 @@ const TEST_USER_SERVICES: UserServices = {
   ],
 };
 
-test("it logs to the console", () => {
-  const consoleLogMock = jest.spyOn(console, "log").mockImplementation();
-  writeEvent(TEST_USER_SERVICES);
-  expect(consoleLogMock).toHaveBeenCalledTimes(1);
-  consoleLogMock.mockRestore();
+const TEST_SQS_RECORD: SQSRecord = {
+  messageId: "19dd0b57-b21e-4ac1-bd88-01bbb068cb78",
+  receiptHandle: "MessageReceiptHandle",
+  body: JSON.stringify(TEST_USER_SERVICES),
+  attributes: {
+    ApproximateReceiveCount: "1",
+    SentTimestamp: "1523232000000",
+    SenderId: "123456789012",
+    ApproximateFirstReceiveTimestamp: "1523232000001",
+  },
+  messageAttributes: {},
+  md5OfBody: "7b270e59b47ff90a553787216d55d91d",
+  eventSource: "aws:sqs",
+  eventSourceARN: "arn:aws:sqs:us-east-1:123456789012:MyQueue",
+  awsRegion: "us-east-1",
+};
+
+const TEST_SQS_EVENT: SQSEvent = {
+  Records: [TEST_SQS_RECORD, TEST_SQS_RECORD],
+};
+
+let consoleMock: jest.SpyInstance;
+const dynamoMock = mockClient(DynamoDBDocumentClient);
+
+describe("writeEvent", () => {
+  beforeEach(() => {
+    consoleMock = jest.spyOn(global.console, "log");
+    dynamoMock.reset();
+
+    process.env.TABLE_NAME = "TABLE_NAME";
+  });
+
+  afterEach(() => {
+    consoleMock.mockRestore();
+    jest.clearAllMocks();
+  });
+
+  test("logs to the console", async () => {
+    await writeEvent(TEST_USER_SERVICES);
+    expect(consoleMock).toHaveBeenCalledTimes(1);
+  });
+
+  test("writes to DynamoDB", async () => {
+    await writeEvent(TEST_USER_SERVICES);
+    expect(dynamoMock.commandCalls(PutCommand).length).toEqual(1);
+  });
+});
+
+describe("lambdaHandler", () => {
+  beforeEach(() => {
+    consoleMock = jest.spyOn(global.console, "log");
+    dynamoMock.reset();
+
+    process.env.TABLE_NAME = "TABLE_NAME";
+  });
+
+  afterEach(() => {
+    consoleMock.mockRestore();
+    jest.clearAllMocks();
+  });
+
+  test("it iterates over each record in the batch", async () => {
+    await lambdaHandler(TEST_SQS_EVENT);
+    expect(consoleMock).toHaveBeenCalledTimes(2);
+    expect(dynamoMock.commandCalls(PutCommand).length).toEqual(2);
+  });
 });

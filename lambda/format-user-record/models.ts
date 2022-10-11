b4fe4f67e81dbb504923a347ddb8afa67740d985
divyaparameswaran <divya.parameswaran@digital.cabinet-office.gov.uk>
2022-10-11 16:58:18 +0100

    Update formatting in the model file
    Signed-off-by: divyaparameswaran <divya.parameswaran@digital.cabinet-office.gov.uk>

--- lambda/format-user-record/models.ts
+++ lambda/format-user-record/models.ts
@@ -6,22 +6,26 @@ export interface UserRecordEvent{
     ServiceList :  Service []
 
 }
-  export interface Service {
+
+export interface Service {
     client_id: ClientId;
     count_successful_logins: Number;
     last_accessed: Date;
 }
+
 export interface TxmaEventBody {
     event_name: String;
     timestamp: Date;
     client_id: ClientId;
     component_id: String; 
     user: UserData;
-  }
-  interface UserData {
+}
+
+interface UserData {
     "user_id": UrnFdnSub
-  }
-  export interface UserServices {
+}
+
+export interface UserServices {
     user_id: UrnFdnSub;
     services: Service[];
-  }
+}

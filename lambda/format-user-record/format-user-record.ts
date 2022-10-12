acf9b25f224fb99f8541cdc9ddddc459806851d0
divyaparameswaran <divya.parameswaran@digital.cabinet-office.gov.uk>
2022-10-12 12:34:27 +0100

    Update the lambda and test with eslint fixes
    Signed-off-by: divyaparameswaran <divya.parameswaran@digital.cabinet-office.gov.uk>

--- lambda/format-user-record/format-user-record.ts
+++ lambda/format-user-record/format-user-record.ts
@@ -1,75 +1,92 @@
-import { SendMessageCommand, SendMessageRequest, SQSClient } from '@aws-sdk/client-sqs';
-import { SQSEvent, SQSRecord } from 'aws-lambda';
-import { UserRecordEvent, Service, UserServices } from './models';
+import {
+  SendMessageCommand,
+  SendMessageRequest,
+  SQSClient,
+} from "@aws-sdk/client-sqs";
+import { SQSEvent, SQSRecord } from "aws-lambda";
+import { UserRecordEvent, Service, UserServices } from "./models";
 
 const QUEUE_URL = "";
-const AWS_REGION = process.env.AWS_REGION;
+const { AWS_REGION } = process.env;
 
-export const handler = async (event: SQSEvent): Promise<void> => {
-    for (const record of event.Records) {
-        //validate the record
-        const validationResponse = await validateSQSRecord(record as SQSRecord);
-        if (!validationResponse) {
-            console.log("[ERROR] Validation error ")
-        }
-        else {
-            const userRecordEvent: UserRecordEvent =  JSON.parse(record.body);
-            for(const service of userRecordEvent.ServiceList){ 
-                if(await  matchService(service,userRecordEvent)){
-                    updateServiceDetails(service,userRecordEvent)
-                    
-                }
-                else {
-                    addNewService(userRecordEvent);
-                    break;
-                }
-        }
-          const messageId =  await  sendSqsMessage(createUserService(userRecordEvent),QUEUE_URL);
-          console.log("[Message sent to QUEUE] with message id = " + messageId);
-        }    
-    }
+export const validateSQSRecord = async (
+  record: SQSRecord
+): Promise<boolean> => {
+  console.log(`Validating the SQS record body${record.body}`);
+  return true;
+};
+export const matchService = async (
+  service: Service,
+  userRecordEvent: UserRecordEvent
+): Promise<boolean> => {
+  return service.client_id === userRecordEvent.TxmaEventBody.client_id;
 };
 
-export const matchService = async (service: Service, userRecordEvent: UserRecordEvent): Promise<Boolean> =>{  
-        return (service.client_id === userRecordEvent.TxmaEventBody.client_id);
-   } ; 
-  
-export const updateServiceDetails =async (service: Service, userRecordEvent: UserRecordEvent) => {
-    service.count_successful_logins = service.count_successful_logins.valueOf() + 1;
-    service.last_accessed = userRecordEvent.TxmaEventBody.timestamp;
-}
+/* eslint-disable no-param-reassign */
+export const updateServiceDetails = async (
+  service: Service,
+  userRecordEvent: UserRecordEvent
+) => {
+  service.count_successful_logins =
+    service.count_successful_logins.valueOf() + 1;
+  service.last_accessed = userRecordEvent.TxmaEventBody.timestamp;
+};
 
 export const addNewService = async (userRecordEvent: UserRecordEvent) => {
-    const newService:Service = { 
-        client_id: userRecordEvent.TxmaEventBody.client_id,
-        count_successful_logins: 1,
-        last_accessed: userRecordEvent.TxmaEventBody.timestamp
-    };    
-    userRecordEvent.ServiceList.push(newService); 
-}
+  const newService: Service = {
+    client_id: userRecordEvent.TxmaEventBody.client_id,
+    count_successful_logins: 1,
+    last_accessed: userRecordEvent.TxmaEventBody.timestamp,
+  };
+  userRecordEvent.ServiceList.push(newService);
+};
 
-export const createUserService = async (userRecordEvent: UserRecordEvent): Promise<UserServices> => {
-    const userService:UserServices = {
-        user_id : userRecordEvent.TxmaEventBody.user.user_id,
-        services: userRecordEvent.ServiceList
-    };
+export const createUserService = async (
+  userRecordEvent: UserRecordEvent
+): Promise<UserServices> => {
+  const userService: UserServices = {
+    user_id: userRecordEvent.TxmaEventBody.user.user_id,
+    services: userRecordEvent.ServiceList,
+  };
 
-    return userService;
-}
+  return userService;
+};
 
-export const validateSQSRecord = async(record: SQSRecord): Promise<Boolean> => {
-    return true;
-}
 export const sendSqsMessage = async (
-    messageBody: object,
-    queueUrl: string
-  ): Promise<string | undefined> => {
-    const client = new SQSClient({ region: AWS_REGION})
-    const message: SendMessageRequest = {
-      QueueUrl: queueUrl,
-      MessageBody: JSON.stringify(messageBody)
+  messageBody: object,
+  queueUrl: string
+): Promise<string | undefined> => {
+  const client = new SQSClient({ region: AWS_REGION });
+  const message: SendMessageRequest = {
+    QueueUrl: queueUrl,
+    MessageBody: JSON.stringify(messageBody),
+  };
+  const result = await client.send(new SendMessageCommand(message));
+  return result.MessageId;
+};
+/* eslint no-await-in-loop: "off" */
+export const handler = async (event: SQSEvent): Promise<void> => {
+  // eslint-disable-next-line no-restricted-syntax
+  for (const record of event.Records) {
+    const validationResponse = await validateSQSRecord(record as SQSRecord);
+    if (!validationResponse) {
+      console.log("[ERROR] Validation error ");
+    } else {
+      const userRecordEvent: UserRecordEvent = JSON.parse(record.body);
+      // eslint-disable-next-line no-restricted-syntax
+      for (const service of userRecordEvent.ServiceList) {
+        if (await matchService(service, userRecordEvent)) {
+          updateServiceDetails(service, userRecordEvent);
+        } else {
+          addNewService(userRecordEvent);
+          break;
+        }
+      }
+      const messageId = await sendSqsMessage(
+        createUserService(userRecordEvent),
+        QUEUE_URL
+      );
+      console.log(`[Message sent to QUEUE] with message id = ${messageId}`);
     }
-    const result = await client.send(new SendMessageCommand(message))
-    return result.MessageId
   }
-
+};
--- lambda/format-user-record/tests/format-user-record.test.ts
+++ lambda/format-user-record/tests/format-user-record.test.ts
@@ -1,73 +1,73 @@
-
 import { SQSEvent, SQSRecord } from "aws-lambda";
-import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs'
-import { UserRecordEvent, TxmaEventBody, UserServices, Service } from '../models';
-import { mockClient } from 'aws-sdk-client-mock'  
+import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
+import { mockClient } from "aws-sdk-client-mock";
 import {
-    addNewService,
-    createUserService,
-    handler,
-    matchService,
-    sendSqsMessage,
-    updateServiceDetails,
-    validateSQSRecord
-  } from "../format-user-record";
- 
+  UserRecordEvent,
+  TxmaEventBody,
+  UserServices,
+  Service,
+} from "../models";
+import {
+  addNewService,
+  createUserService,
+  handler,
+  matchService,
+  sendSqsMessage,
+  updateServiceDetails,
+  validateSQSRecord,
+} from "../format-user-record";
+
 const TXMA_EVENT_BODY: TxmaEventBody = {
-    event_name: "event_1",
-    timestamp: new Date(),
-    client_id: "client_id_3",
-    component_id: "component_id",
-    user: {
-        user_id:"123456"
-    }
+  event_name: "event_1",
+  timestamp: new Date(),
+  client_id: "client_id_3",
+  component_id: "component_id",
+  user: {
+    user_id: "123456",
+  },
 };
+
 const TXMA_EVENT_BODY_OTHER: TxmaEventBody = {
   event_name: "event_1",
   timestamp: new Date(),
   client_id: "client_id_1",
   component_id: "component_id",
   user: {
-      user_id:"123456"
-  }
-};
-const QUEUE_URL = "";
-const service1: Service ={
-    client_id: "client_id_1",
-    count_successful_logins: 1,
-    last_accessed: new Date() 
+    user_id: "123456",
+  },
 };
 
-const service2: Service ={
-    client_id: "client_id_2",
-    count_successful_logins: 3,
-    last_accessed: new Date() 
+const service1: Service = {
+  client_id: "client_id_1",
+  count_successful_logins: 1,
+  last_accessed: new Date(),
 };
-const userService: UserServices ={
-    user_id:"123456",
-    services: [
-        service1,
-        service2
-    ]
-}
+
+const service2: Service = {
+  client_id: "client_id_2",
+  count_successful_logins: 3,
+  last_accessed: new Date(),
+};
+
+const userService: UserServices = {
+  user_id: "123456",
+  services: [service1, service2],
+};
+
 const TEST_USER_RECORD: UserRecordEvent = {
-    TxmaEventBody: TXMA_EVENT_BODY,
-    ServiceList: [
-        service1,
-        service2
-    ]
+  TxmaEventBody: TXMA_EVENT_BODY,
+  ServiceList: [service1, service2],
 };
+
 const TEST_USER_RECORD_OTHER: UserRecordEvent = {
   TxmaEventBody: TXMA_EVENT_BODY_OTHER,
-  ServiceList: [
-      service1,
-      service2
-  ]
+  ServiceList: [service1, service2],
 };
+
 const TEST_SQS_RECORD: SQSRecord = {
   messageId: "19dd0b57-b21e-4ac1-bd88-01bbb068cb78",
   receiptHandle: "MessageReceiptHandle",
-  body:JSON.stringify(TEST_USER_RECORD) ,
+  body: JSON.stringify(TEST_USER_RECORD),
   attributes: {
     ApproximateReceiveCount: "1",
     SentTimestamp: "1523232000000",
@@ -82,76 +82,81 @@ const TEST_SQS_RECORD: SQSRecord = {
 };
 
 const TEST_SQS_EVENT: SQSEvent = {
-    Records: [TEST_SQS_RECORD],
+  Records: [TEST_SQS_RECORD],
 };
 
-const MOCK_MESSAGE_ID = 'MyMessageId';
+const MOCK_MESSAGE_ID = "MyMessageId";
 
-const MOCK_QUEUE_URL = 'http://my_queue_url';
+const MOCK_QUEUE_URL = "http://my_queue_url";
 
 const sqsMock = mockClient(SQSClient);
 
-describe("validateSQSRecord", () => {  
+describe("validateSQSRecord", () => {
   test("validate SQS record to true", async () => {
     expect(validateSQSRecord(TEST_SQS_RECORD)).toBeTruthy();
   });
 });
 
-describe("createUserService", () => {  
-    test("creates a new user service", async () => {
-    const  userService: UserServices =  await createUserService(TEST_USER_RECORD);
-      expect(userService.user_id).toEqual(TEST_USER_RECORD.TxmaEventBody.user.user_id);
-      expect(userService.services).toEqual(TEST_USER_RECORD.ServiceList);
-    });
+describe("createUserService", () => {
+  test("creates a new user service", async () => {
+    const newuserService: UserServices = await createUserService(
+      TEST_USER_RECORD
+    );
+    expect(newuserService.user_id).toEqual(
+      TEST_USER_RECORD.TxmaEventBody.user.user_id
+    );
+    expect(newuserService.services).toEqual(TEST_USER_RECORD.ServiceList);
   });
-  
-  describe("addNewService", () => {
-    test("adds a new service to the service list", async() => {
-        await addNewService(TEST_USER_RECORD);
-        expect(TEST_USER_RECORD.ServiceList.length).toEqual(3)
-    });
-  });
+});
 
-  describe("updateServiceDetails", () => {
-    test("Update Service details", async() => {
-        const count:number = service1.count_successful_logins.valueOf();
-        await updateServiceDetails(service1,TEST_USER_RECORD);
-        expect(service1.count_successful_logins).toEqual(count+1);
-        expect(service1.last_accessed).toEqual(TEST_USER_RECORD.TxmaEventBody.timestamp);
-    });
+describe("addNewService", () => {
+  test("adds a new service to the service list", async () => {
+    await addNewService(TEST_USER_RECORD);
+    expect(TEST_USER_RECORD.ServiceList.length).toEqual(3);
   });
-  
-  describe("matchService", () => {
-    test("Match false for the client Id from Service list to the one on TXMA event", async() => {
-           const matched =  await matchService(service1,TEST_USER_RECORD);
-           expect(matched).toBeFalsy();
-        
-    });
-    test("Match true forthe client Id from Service list to the one on TXMA event", async() => {
-      const matched =  await matchService(service1,TEST_USER_RECORD_OTHER);
-      expect(matched).toBeTruthy();
-   
-    });
-  });
+});
 
-  describe("sendSqsMessage", () => {  
-    beforeEach(() => {
-      sqsMock.reset();
-    });
-    afterEach(() => {
-      jest.clearAllMocks();
-    });
-    test("Send the SQS event on the queue", async () => {
-      sqsMock.on(SendMessageCommand).resolves({ MessageId: MOCK_MESSAGE_ID });
-      const messageId = await sendSqsMessage(userService, MOCK_QUEUE_URL);
-      expect(messageId).toEqual(MOCK_MESSAGE_ID)
-      expect(sqsMock.commandCalls(SendMessageCommand,{
+describe("updateServiceDetails", () => {
+  test("Update Service details", async () => {
+    const count: number = service1.count_successful_logins.valueOf();
+    await updateServiceDetails(service1, TEST_USER_RECORD);
+    expect(service1.count_successful_logins).toEqual(count + 1);
+    expect(service1.last_accessed).toEqual(
+      TEST_USER_RECORD.TxmaEventBody.timestamp
+    );
+  });
+});
+
+describe("matchService", () => {
+  test("Match false for the client Id from Service list to the one on TXMA event", async () => {
+    const matched = await matchService(service1, TEST_USER_RECORD);
+    expect(matched).toBeFalsy();
+  });
+  test("Match true forthe client Id from Service list to the one on TXMA event", async () => {
+    const matched = await matchService(service1, TEST_USER_RECORD_OTHER);
+    expect(matched).toBeTruthy();
+  });
+});
+
+describe("sendSqsMessage", () => {
+  beforeEach(() => {
+    sqsMock.reset();
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  test("Send the SQS event on the queue", async () => {
+    sqsMock.on(SendMessageCommand).resolves({ MessageId: MOCK_MESSAGE_ID });
+    const messageId = await sendSqsMessage(userService, MOCK_QUEUE_URL);
+    expect(messageId).toEqual(MOCK_MESSAGE_ID);
+    expect(
+      sqsMock.commandCalls(SendMessageCommand, {
         QueueUrl: MOCK_QUEUE_URL,
-        MessageBody: JSON.stringify(userService)
-      }));  
-    });
+        MessageBody: JSON.stringify(userService),
+      })
+    );
   });
-
+});
 
 describe("handler", () => {
   beforeEach(() => {
@@ -160,13 +165,14 @@ describe("handler", () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
-  test("format method and send on SQS queue", async() => {
-      sqsMock.on(SendMessageCommand).resolves({ MessageId: MOCK_MESSAGE_ID });
-      await handler(TEST_SQS_EVENT);    
-      expect(sqsMock.commandCalls(SendMessageCommand,{
+  test("format method and send on SQS queue", async () => {
+    sqsMock.on(SendMessageCommand).resolves({ MessageId: MOCK_MESSAGE_ID });
+    await handler(TEST_SQS_EVENT);
+    expect(
+      sqsMock.commandCalls(SendMessageCommand, {
         QueueUrl: MOCK_QUEUE_URL,
-        MessageBody: JSON.stringify(userService)
-      }));  
+        MessageBody: JSON.stringify(userService),
+      })
+    );
   });
 });
-

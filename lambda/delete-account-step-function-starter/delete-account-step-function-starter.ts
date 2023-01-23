a8947ad076678d1826f917e3ffb2f548f4cfd05e
Ana Hickmann <ana.hickmann@digital.cabinet-office.gov.uk>
2023-01-23 15:53:04 +0000

    GUA-595: Spike 2 - Added unit tests and fixed template.yaml
    Signed-off-by: Ana Hickmann <ana.hickmann@digital.cabinet-office.gov.uk>

--- lambda/delete-account-step-function-starter/delete-account-step-function-starter.ts
+++ lambda/delete-account-step-function-starter/delete-account-step-function-starter.ts
@@ -1,27 +1,48 @@
 import { SNSEvent } from "aws-lambda";
-import { SFNClient, StartExecutionCommand } from "@aws-sdk/client-sfn";
+import {
+  SFNClient,
+  StartExecutionCommand,
+  StartExecutionCommandOutput,
+} from "@aws-sdk/client-sfn";
 import { SNSMessage } from "./models";
+
+const sfnClient = new SFNClient({ region: "eu-west-2" });
+
+export const validateSNSMessage = (snsMessage: SNSMessage): SNSMessage => {
+  if (
+    !snsMessage.email ||
+    !snsMessage.access_token ||
+    !snsMessage.public_subject_id
+  ) {
+    throw new Error(
+      "SNS message is missing one or more of the required attributes 'email', 'access_token' 'public_subject_id'"
+    );
+  }
+
+  return snsMessage;
+};
+
+export const startStateMachine = async (
+  snsMessage: SNSMessage
+): Promise<StartExecutionCommandOutput> => {
+  console.log("Starting state machine execution.");
+
+  const command = new StartExecutionCommand({
+    stateMachineArn: process.env.STEP_FUNCTION_ARN!,
+    input: JSON.stringify(snsMessage),
+  });
+  return sfnClient.send(command);
+};
 
 export const handler = async (event: SNSEvent): Promise<void> => {
   console.log(`Received SNS Event: ${JSON.stringify(event)}`);
 
-  const client = new SFNClient({ region: "eu-west-2" });
-
   await Promise.all(
     event.Records.map(async (record) => {
+      const snsMessage: SNSMessage = JSON.parse(record.Sns.Message);
+      validateSNSMessage(snsMessage);
       try {
-        const snsMessage: SNSMessage = JSON.parse(record.Sns.Message);
-        console.log(`Parsed SNS Message: ${JSON.stringify(snsMessage)}`);
-
-        const input = {
-          stateMachineArn: process.env.STEP_FUNCTION_ARN!,
-          input: JSON.stringify(snsMessage),
-        };
-
-        console.log("Starting state machine execution.");
-
-        const command = new StartExecutionCommand(input);
-        const response = await client.send(command);
+        const response = await startStateMachine(snsMessage);
         console.log(
           `Response from StartExecutionCommand: ${JSON.stringify(response)}`
         );
---
+++ lambda/delete-account-step-function-starter/tests/delete-account-step-function-starter.test.ts
@@ -0,0 +1,115 @@
+import "aws-sdk-client-mock-jest";
+import { SFNClient, StartExecutionCommand } from "@aws-sdk/client-sfn";
+import { mockClient } from "aws-sdk-client-mock";
+import {
+  handler,
+  startStateMachine,
+  validateSNSMessage,
+} from "../delete-account-step-function-starter";
+
+import { TEST_USER_DATA, TEST_SNS_EVENT } from "./test-helpers";
+
+const sfnMock = mockClient(SFNClient);
+
+describe("handler", () => {
+  beforeEach(() => {
+    sfnMock.reset();
+    process.env.STEP_FUNCTION_ARN = "STEP_FUNCTION_ARN";
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("if it iterates over each SNS record in the batch", async () => {
+    await handler(TEST_SNS_EVENT);
+    expect(sfnMock.commandCalls(StartExecutionCommand).length).toEqual(2);
+    // expect(validateSNSMessage).toHaveBeenCalledTimes(2)
+  });
+
+  describe("handler error handling", () => {
+    let consoleErrorMock: jest.SpyInstance;
+
+    beforeEach(() => {
+      consoleErrorMock = jest
+        .spyOn(console, "error")
+        .mockImplementation(() => {});
+      sfnMock.rejectsOnce("mock error");
+    });
+
+    afterAll(() => {
+      consoleErrorMock.mockRestore();
+    });
+
+    afterEach(() => {
+      consoleErrorMock.mockClear();
+    });
+
+    test("if the handler logs and throws an error when the StartExecutionCommand throws an error", async () => {
+      await expect(async () => {
+        await handler(TEST_SNS_EVENT);
+      }).rejects.toThrowError();
+      expect(consoleErrorMock).toHaveBeenCalledTimes(1);
+    });
+  });
+});
+
+describe("validateSNSMessage", () => {
+  test("that it doesn't throw an error when the SNS message is valid", () => {
+    expect(validateSNSMessage(TEST_USER_DATA)).toBe(TEST_USER_DATA);
+  });
+
+  test("that it throws an error when the SNS message is missing the required attribute 'email'", () => {
+    const snsMessage = JSON.parse(
+      JSON.stringify({
+        user_id: "user-id",
+        access_token: "access_token",
+        source_ip: "source_ip",
+        persistent_session_id: "persistent_session_id",
+        session_id: "session_id",
+        public_subject_id: "public_subject_id",
+        legacy_subject_id: "legacy_subject_id",
+      })
+    );
+    expect(() => {
+      validateSNSMessage(snsMessage);
+    }).toThrowError(
+      "SNS message is missing one or more of the required attributes 'email', 'access_token' 'public_subject_id'"
+    );
+  });
+
+  test("that it doesn't throw an error when the SNS message is missing a non-required attribute", () => {
+    const snsMessage = JSON.parse(
+      JSON.stringify({
+        user_id: "user-id",
+        access_token: "access_token",
+        email: "email",
+        source_ip: "source_ip",
+        persistent_session_id: "persistent_session_id",
+        session_id: "session_id",
+        public_subject_id: "public_subject_id",
+      })
+    );
+    expect(validateSNSMessage(snsMessage)).toBe(snsMessage);
+  });
+});
+
+describe("startStateMachine", () => {
+  beforeEach(() => {
+    sfnMock.reset();
+
+    process.env.STEP_FUNCTION_ARN = "STEP_FUNCTION_ARN";
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("if the StartExecutionCommand gets called called with the correct input", async () => {
+    await startStateMachine(TEST_USER_DATA);
+    expect(sfnMock).toHaveReceivedCommandWith(StartExecutionCommand, {
+      stateMachineArn: process.env.STEP_FUNCTION_ARN,
+      input: JSON.stringify(TEST_USER_DATA),
+    });
+  });
+});

67260024b443d46add2814357957385918c31f19
Divya <divya.parameswaran@digital.cabinet-office.gov.uk>
2023-05-10 11:14:06 +0100

    GUA-709: Add Lambda and tests for email subscriptions

---
+++ lambda/delete-email-subscriptions/tests/test-helpers.ts
@@ -0,0 +1,37 @@
+import type { SNSEvent, SNSMessage, SNSEventRecord } from "aws-lambda";
+
+export const TEST_USER_DATA = {
+  user_id: "user-id",
+  access_token: "access_token",
+  email: "email",
+  source_ip: "source_ip",
+  persistent_session_id: "persistent_session_id",
+  session_id: "session_id",
+  public_subject_id: "public_subject_id",
+  legacy_subject_id: "legacy_subject_id",
+};
+
+const TEST_SNS_MESSAGE: SNSMessage = {
+  SignatureVersion: "SignatureVersion",
+  Timestamp: "Timestamp",
+  Signature: "Signature",
+  SigningCertUrl: "SigningCertUrl",
+  MessageId: "MessageId",
+  Message: JSON.stringify(TEST_USER_DATA),
+  MessageAttributes: {},
+  Type: "Type",
+  UnsubscribeUrl: "unsubscribeUrl",
+  TopicArn: "TopicArn",
+  Subject: "Subject",
+};
+
+const TEST_SNS_EVENT_RECORD: SNSEventRecord = {
+  EventVersion: "1",
+  EventSubscriptionArn: "arn",
+  EventSource: "source",
+  Sns: TEST_SNS_MESSAGE,
+};
+
+export const TEST_SNS_EVENT: SNSEvent = {
+  Records: [TEST_SNS_EVENT_RECORD],
+};

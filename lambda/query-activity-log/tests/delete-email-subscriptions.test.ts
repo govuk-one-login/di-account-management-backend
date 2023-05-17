ed90314ee6802f6fffd0dfcac44405ccddc15213
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2023-05-17 12:31:02 +0100

    GUA-748: Naievely copy across all Lambda files from delete-email-subscriptions
    We want to bootstrap a new lambda based on our latest template, work was
    done recently to set that up with delete-email-subscriptions with node
    v18.
    
    I'm copy across everything here and will follow up with commits to make
    clear where I'm expecting changes or where code only exists because it's
    a bootstrapped template

---
+++ lambda/query-activity-log/tests/delete-email-subscriptions.test.ts
@@ -0,0 +1,81 @@
+import {
+  handler,
+  getRequestConfig,
+  validateUserData,
+} from "../delete-email-subscriptions";
+import { TEST_USER_DATA, TEST_SNS_EVENT } from "./test-helpers";
+
+describe("handler", () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("that it successfully processes the SNS message", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires,global-require
+    const module = require("../delete-email-subscriptions");
+    const validateUserDataMock = jest
+      .spyOn(module, "validateUserData")
+      .mockReturnValueOnce("validateUserData-mock");
+
+    const deleteEmailSubscriptionMock = jest
+      .spyOn(module, "deleteEmailSubscription")
+      .mockReturnValue("deleteEmailSubscription-mock");
+
+    await expect(handler(TEST_SNS_EVENT)).resolves.not.toThrowError();
+    expect(validateUserDataMock).toHaveBeenCalledTimes(1);
+    expect(validateUserDataMock).toHaveBeenCalledWith(TEST_USER_DATA);
+    expect(deleteEmailSubscriptionMock).toHaveBeenCalledTimes(1);
+    expect(deleteEmailSubscriptionMock).toHaveBeenCalledWith(TEST_USER_DATA);
+  });
+});
+
+describe("getRequestConfig", () => {
+  test("that it returns the request config in the correct format", () => {
+    expect(getRequestConfig("TOKEN")).toEqual({
+      headers: { Authorization: "Bearer TOKEN" },
+      method: "DELETE",
+    });
+  });
+});
+
+describe("validateUserData", () => {
+  test("that it does not throw an error when the SNS message is valid", () => {
+    expect(validateUserData(TEST_USER_DATA)).toBe(TEST_USER_DATA);
+  });
+
+  test("that it throws an error if the SNS message is missing the required attribute public_subject_id", () => {
+    const userData = JSON.parse(
+      JSON.stringify({
+        user_id: "user-id",
+        access_token: "access_token",
+        email: "email",
+        source_ip: "source_ip",
+        persistent_session_id: "persistent_session_id",
+        session_id: "session_id",
+        legacy_subject_id: "legacy_subject_id",
+      })
+    );
+    expect(() => {
+      validateUserData(userData);
+    }).toThrowError(`userData is not valid : ${JSON.stringify(userData)}`);
+  });
+
+  test("that it does not throw an error if the SNS message is missing the non-required attribute legacy_subject_id", () => {
+    const userData = JSON.parse(
+      JSON.stringify({
+        user_id: "user-id",
+        access_token: "access_token",
+        email: "email",
+        source_ip: "source_ip",
+        persistent_session_id: "persistent_session_id",
+        session_id: "session_id",
+        public_subject_id: "public_subject_id",
+      })
+    );
+    expect(validateUserData(userData)).toBe(userData);
+  });
+});

cf9110bb575bd2c9033df1cdea32e1f72d7d5c62
Divya <divya.parameswaran@digital.cabinet-office.gov.uk>
2023-05-19 17:17:42 +0100

    GUA-748: Add tests for query activity log Query Lambda
    - Add validation tests
    - Add tests for sending SQS message

--- lambda/query-activity-log/tests/query-activity-log.test.ts
+++ lambda/query-activity-log/tests/query-activity-log.test.ts
@@ -1,3 +1,191 @@
+import { mockClient } from "aws-sdk-client-mock";
+import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
+import {
+  sendSqsMessage,
+  validateTxmaEventBody,
+  validateUser,
+} from "../query-activity-log";
+import {
+  TEST_TXMA_EVENT,
+  clientId,
+  eventType,
+  messageId,
+  queueUrl,
+  sessionId,
+  timestamp,
+  userId,
+} from "./test-helpers";
+import {
+  Activity,
+  ActivityLogEntry,
+  UserActivityLog,
+} from "../../shared-models";
+
+const sqsMock = mockClient(SQSClient);
+const activityList: Activity[] = [
+  {
+    type: "service_visited",
+    client_id: clientId,
+    timestamp,
+  },
+];
+
+const activityLogEntry: ActivityLogEntry = {
+  event_type: eventType,
+  session_id: sessionId,
+  user_id: userId,
+  timestamp,
+  activities: activityList,
+  truncated: true,
+};
+const userActivityLog: UserActivityLog = {
+  TxmaEvent: TEST_TXMA_EVENT,
+  ActivityLogEntry: activityLogEntry,
+};
+
+describe("validateTxmaEventBody", () => {
+  test("doesn't throw an error with valid txma data", () => {
+    expect(validateTxmaEventBody(TEST_TXMA_EVENT)).toBe(undefined);
+  });
+  test("throws error when client_id is missing", () => {
+    const txmaEvent = JSON.parse(
+      JSON.stringify({
+        services: [
+          {
+            timestamp: new Date().toISOString,
+            event_name: "AUTH_AUTH_CODE_ISSUED",
+            user: {
+              user_id: userId,
+            },
+          },
+        ],
+      })
+    );
+    expect(() => {
+      validateTxmaEventBody(txmaEvent);
+    }).toThrowError();
+  });
+  test("throws error when timestamp is missing", () => {
+    const txmaEvent = JSON.parse(
+      JSON.stringify({
+        services: [
+          {
+            client_id: clientId,
+            event_name: "AUTH_AUTH_CODE_ISSUED",
+            user: {
+              user_id: userId,
+            },
+          },
+        ],
+      })
+    );
+    expect(() => {
+      validateTxmaEventBody(txmaEvent);
+    }).toThrowError();
+  });
+  test("throws error when event name is missing", () => {
+    const txmaEvent = JSON.parse(
+      JSON.stringify({
+        services: [
+          {
+            client_id: clientId,
+            timestamp: new Date().toISOString,
+            user: {
+              user_id: userId,
+            },
+          },
+        ],
+      })
+    );
+    expect(() => {
+      validateTxmaEventBody(txmaEvent);
+    }).toThrowError();
+  });
+  test(" throws error when user is missing", () => {
+    const txmaEvent = JSON.parse(
+      JSON.stringify({
+        services: [
+          {
+            client_id: clientId,
+            timestamp: new Date().toISOString,
+            event_name: "AUTH_AUTH_CODE_ISSUED",
+          },
+        ],
+      })
+    );
+    expect(() => {
+      validateTxmaEventBody(txmaEvent);
+    }).toThrowError();
+  });
+  test("throws error when user_id  is missing", () => {
+    const txmaEvent = JSON.parse(
+      JSON.stringify({
+        services: [
+          {
+            client_id: clientId,
+            timestamp: new Date().toISOString,
+            event_name: "AUTH_AUTH_CODE_ISSUED",
+            user: {},
+          },
+        ],
+      })
+    );
+    expect(() => {
+      validateTxmaEventBody(txmaEvent);
+    }).toThrowError();
+  });
+
+  test("throws error when session_id  is missing", () => {
+    const txmaEvent = JSON.parse(
+      JSON.stringify({
+        services: [
+          {
+            client_id: clientId,
+            timestamp: new Date().toISOString,
+            event_name: "AUTH_AUTH_CODE_ISSUED",
+            user: {},
+          },
+        ],
+      })
+    );
+    expect(() => {
+      validateTxmaEventBody(txmaEvent);
+    }).toThrowError();
+  });
+});
+
+describe("validateUser", () => {
+  test("throws error when user is is missing", () => {
+    const inValidUser = JSON.parse(JSON.stringify({}));
+    expect(() => {
+      validateUser(inValidUser);
+    }).toThrowError();
+  });
+});
+
+describe("sendSqsMessage", () => {
+  beforeEach(() => {
+    sqsMock.reset();
+    process.env.QUEUE_URL = queueUrl;
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  test("Send the SQS event on the queue", async () => {
+    sqsMock.on(SendMessageCommand).resolves({ MessageId: messageId });
+
+    expect(
+      await sendSqsMessage(JSON.stringify(userActivityLog), queueUrl)
+    ).toEqual(messageId);
+    expect(
+      sqsMock.commandCalls(SendMessageCommand, {
+        QueueUrl: queueUrl,
+        MessageBody: JSON.stringify(Object),
+      })
+    );
+  });
+});
+
 describe("handler", () => {
   beforeEach(() => {
     jest.restoreAllMocks();
---
+++ lambda/query-activity-log/tests/test-helpers.ts
@@ -0,0 +1,82 @@
+import { DynamoDBStreamEvent, DynamoDBRecord } from "aws-lambda";
+import { TxmaEvent, UserData } from "../../shared-models";
+
+export const eventType = "AUTH_AUTH_CODE_ISSUED";
+export const userId = "user_id";
+export const sessionId = "session_id";
+export const clientId = "client_id";
+export const date = new Date();
+export const tableName = "TableName";
+export const messageId = "MyMessageId";
+export const queueUrl = "http://my_queue_url";
+export const timestamp = date.valueOf();
+const timestampFormatted = date.toISOString();
+const govukSigninJourneyId = "abc123";
+
+const user: UserData = {
+  user_id: userId,
+  govuk_signin_journey_id: govukSigninJourneyId,
+  session_id: sessionId,
+};
+const generateTestTxmaEvent = (
+  txmaEventName = "AUTH_AUTH_CODE_ISSUED"
+): TxmaEvent => ({
+  event_id: "event_id",
+  event_name: `${txmaEventName}`,
+  timestamp,
+  timestamp_formatted: timestampFormatted,
+  client_id: clientId,
+  user,
+});
+
+const generateDynamoSteamRecord = (
+  txmaEventName = "AUTH_AUTH_CODE_ISSUED"
+): DynamoDBRecord => ({
+  eventID: "1234567",
+  eventName: "INSERT",
+  dynamodb: {
+    ApproximateCreationDateTime: Date.now(),
+    NewImage: {
+      remove_at: {
+        N: "1676378763",
+      },
+      id: { S: "event-id" },
+      timestamp: { N: `${Date.now()}` },
+      event: {
+        M: {
+          event_id: {
+            S: "event_id",
+          },
+          event_name: {
+            S: `${txmaEventName}`,
+          },
+          timestamp: {
+            N: `${timestamp}`,
+          },
+          timestamp_formatted: {
+            S: timestampFormatted,
+          },
+          client_id: {
+            S: clientId,
+          },
+          user: {
+            M: {
+              user_id: {
+                S: userId,
+              },
+              govuk_signin_journey_id: {
+                S: govukSigninJourneyId,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+});
+
+export const TEST_DYNAMO_STREAM_EVENT: DynamoDBStreamEvent = {
+  Records: [generateDynamoSteamRecord(), generateDynamoSteamRecord()],
+};
+
+export const TEST_TXMA_EVENT: TxmaEvent = generateTestTxmaEvent();

c68889c388d1d84c39be2aed74f7dc375a7acf7e
Divya <divya.parameswaran@digital.cabinet-office.gov.uk>
2023-05-22 21:26:53 +0100

    GUA:748: Move models to be under the lambda directory.

---
+++ lambda/query-activity-log/models.ts
@@ -0,0 +1,43 @@
+type UrnFdnSub = string;
+type ClientId = string;
+type SessionId = string;
+
+export interface UserData {
+  user_id: UrnFdnSub;
+  govuk_signin_journey_id: string;
+  session_id: SessionId;
+}
+
+export interface TxmaEvent {
+  event_id: string;
+  timestamp: number;
+  timestamp_formatted: string;
+  event_name: string;
+  client_id: ClientId;
+  user: UserData;
+}
+
+export interface Activity {
+  type: string;
+  client_id: string;
+  timestamp: number;
+}
+
+export interface ActivityLogEntry {
+  event_type: string;
+  session_id: string;
+  user_id: string;
+  timestamp: number;
+  activities: Activity[];
+  truncated: boolean;
+}
+
+export interface UserActivityLog {
+  TxmaEvent: TxmaEvent;
+  ActivityLogEntry: ActivityLogEntry | undefined;
+}
+
+export const allowedTxmaEvents: Array<string> = [
+  "AUTH_AUTH_CODE_ISSUED",
+  "AUTH_IPV_AUTHORISATION_REQUESTED",
+];

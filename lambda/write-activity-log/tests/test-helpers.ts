fb5eb306525e19583f57891b67afdc1933fa6ce3
Dougal Rees <24165331+DougsCER@users.noreply.github.com>
2023-05-23 14:54:43 +0100

    update table info and add tests

--- lambda/write-activity-log/tests/test-helpers.ts
+++ lambda/write-activity-log/tests/test-helpers.ts
@@ -1,41 +1,30 @@
 import { SQSEvent, SQSRecord } from "aws-lambda";
-import { UserServices } from "../models";
 
-export const date = new Date();
-export const tableName = "TableName";
-export const clientId = "clientId";
-export const userId = "userId";
+import { ActivityLogEntry } from "../../shared/models";
+import { TooManyEntriesInBatchRequest } from "@aws-sdk/client-sqs";
 
-export const TEST_USER_SERVICES: UserServices = {
+// Activity log entry constants
+export const event_type = "TXMA_EVENT";
+export const sessionId = "123456789";
+export const userId = "qwerty";
+export const timestamp = 123456789;
+export const truncated = false;
+
+// Activity constants
+export const clientId = "client-id-value";
+export const type = "activity-type-value";
+
+export const TEST_ACTIVITY_LOG_ENTRY: ActivityLogEntry = {
+  event_type: event_type,
+  session_id: sessionId,
   user_id: userId,
-  services: [
+  timestamp: timestamp,
+  truncated: truncated,
+  activities: [
     {
       client_id: clientId,
-      last_accessed: date.valueOf(),
-      last_accessed_pretty: new Intl.DateTimeFormat("en-GB", {
-        dateStyle: "long",
-      }).format(date),
-      count_successful_logins: 1,
-    },
+      timestamp: timestamp,
+      type: type
+    }
   ],
-};
-
-const TEST_SQS_RECORD: SQSRecord = {
-  messageId: "19dd0b57-b21e-4ac1-bd88-01bbb068cb78",
-  receiptHandle: "MessageReceiptHandle",
-  body: JSON.stringify(TEST_USER_SERVICES),
-  attributes: {
-    ApproximateReceiveCount: "1",
-    SentTimestamp: "1523232000000",
-    SenderId: "123456789012",
-    ApproximateFirstReceiveTimestamp: "1523232000001",
-  },
-  messageAttributes: {},
-  md5OfBody: "7b270e59b47ff90a553787216d55d91d",
-  eventSource: "aws:sqs",
-  eventSourceARN: "arn:aws:sqs:us-east-1:123456789012:MyQueue",
-  awsRegion: "us-east-1",
-};
-export const TEST_SQS_EVENT: SQSEvent = {
-  Records: [TEST_SQS_RECORD, TEST_SQS_RECORD],
 };

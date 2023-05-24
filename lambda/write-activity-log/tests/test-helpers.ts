1fee6a361c1ca556b16bd3b3858367871a237644
Dougal Rees <24165331+DougsCER@users.noreply.github.com>
2023-05-24 18:47:51 +0100

    unit tests

--- lambda/write-activity-log/tests/test-helpers.ts
+++ lambda/write-activity-log/tests/test-helpers.ts
@@ -31,18 +31,32 @@ var MUTABLE_ACTIVITY_LOG_ENTRY: ActivityLogEntry = {
 
 export const TEST_ACTIVITY_LOG_ENTRY: ActivityLogEntry = MUTABLE_ACTIVITY_LOG_ENTRY;
 
-const NO_ACTIVITY_ARRAY = { 
-  ...MUTABLE_ACTIVITY_LOG_ENTRY, activities: undefined };
-export const ACTIVITY_LOG_ENTRY_NO_ACTIVITY_ARRAY: ActivityLogEntry = 
-  JSON.parse(JSON.stringify(NO_ACTIVITY_ARRAY));
+const NO_ACTIVITY_ARRAY = { ...MUTABLE_ACTIVITY_LOG_ENTRY, activities: undefined };
+export const ACTIVITY_LOG_ENTRY_NO_ACTIVITY_ARRAY: ActivityLogEntry = JSON.parse(JSON.stringify(NO_ACTIVITY_ARRAY));
 
 
-const NO_USER_ID = { 
-  ...MUTABLE_ACTIVITY_LOG_ENTRY, user_id: undefined };
-export const ACTIVITY_LOG_ENTRY_NO_USER_ID: ActivityLogEntry = 
-  JSON.parse(JSON.stringify(NO_USER_ID));
+const NO_USER_ID = {   ...MUTABLE_ACTIVITY_LOG_ENTRY, user_id: undefined };
+export const ACTIVITY_LOG_ENTRY_NO_USER_ID: ActivityLogEntry = JSON.parse(JSON.stringify(NO_USER_ID));
 
-const NO_TIMESTAMP = { 
-    ...MUTABLE_ACTIVITY_LOG_ENTRY, timestamp: undefined };
-export const ACTIVITY_LOG_ENTRY_NO_TIMESTAMP: ActivityLogEntry = 
-  JSON.parse(JSON.stringify(NO_TIMESTAMP));
+const NO_TIMESTAMP = { ...MUTABLE_ACTIVITY_LOG_ENTRY, timestamp: undefined };
+export const ACTIVITY_LOG_ENTRY_NO_TIMESTAMP: ActivityLogEntry = JSON.parse(JSON.stringify(NO_TIMESTAMP));
+
+const TEST_SQS_RECORD: SQSRecord = {
+  messageId: "19dd0b57-b21e-4ac1-bd88-01bbb068cb78",
+  receiptHandle: "MessageReceiptHandle",
+  body: JSON.stringify(TEST_ACTIVITY_LOG_ENTRY),
+  attributes: {
+    ApproximateReceiveCount: "1",
+    SentTimestamp: "1523232000000",
+    SenderId: "123456789012",
+    ApproximateFirstReceiveTimestamp: "1523232000001",
+  },
+  messageAttributes: {},
+  md5OfBody: "7b270e59b47ff90a553787216d55d91d",
+  eventSource: "aws:sqs",
+  eventSourceARN: "arn:aws:sqs:us-east-1:123456789012:MyQueue",
+  awsRegion: "us-east-1",
+};
+export const TEST_SQS_EVENT: SQSEvent = {
+  Records: [TEST_SQS_RECORD, TEST_SQS_RECORD],
+};
--- lambda/write-activity-log/tests/write-activity-log.test.ts
+++ lambda/write-activity-log/tests/write-activity-log.test.ts
@@ -1,10 +1,21 @@
-import { validateActivityLogEntry
-    } from "../write-activity-log"
-import { ACTIVITY_LOG_ENTRY_NO_TIMESTAMP,
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
+import { mockClient } from "aws-sdk-client-mock";
+import { 
+    validateActivityLogEntry,
+    writeActivityLogEntry,
+    handler
+} from "../write-activity-log"
+import { 
+    ACTIVITY_LOG_ENTRY_NO_TIMESTAMP,
     ACTIVITY_LOG_ENTRY_NO_USER_ID,
     TEST_ACTIVITY_LOG_ENTRY,
-    ACTIVITY_LOG_ENTRY_NO_ACTIVITY_ARRAY
+    ACTIVITY_LOG_ENTRY_NO_ACTIVITY_ARRAY,
+    TEST_SQS_EVENT
 } from "./test-helpers";
+
+const dynamoMock = mockClient(DynamoDBDocumentClient);
+const sqsMock = mockClient(SQSClient);
 
 describe("ValidateActivityLogEntries", () => {
 
@@ -36,6 +47,62 @@ describe("ValidateActivityLogEntries", () => {
             .toThrowError(
             new Error(`Could not validate activity log entry ${JSON.stringify(ACTIVITY_LOG_ENTRY_NO_TIMESTAMP)}`)
         )
-    });
-
+    })
 })
+
+describe("writeActivitwriteActivityLogEntryyLog", () => {
+    beforeEach(() => {
+      dynamoMock.reset();
+    });
+  
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+  
+    test("writes to DynamoDB", async () => {
+      await writeActivityLogEntry(TEST_ACTIVITY_LOG_ENTRY);
+      expect(dynamoMock.commandCalls(PutCommand).length).toEqual(1);
+    });
+});
+
+describe("lambdaHandler", () => {
+    beforeEach(() => {
+      dynamoMock.reset();
+      sqsMock.reset();
+      //process.env.TABLE_NAME = tableName;
+    });
+  
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+  
+    test("it iterates over each record in the batch", async () => {
+      await handler(TEST_SQS_EVENT);
+      expect(dynamoMock.commandCalls(PutCommand).length).toEqual(2);
+    });
+  
+    describe("error handling", () => {
+      let consoleErrorMock: jest.SpyInstance;
+  
+      beforeEach(() => {
+        consoleErrorMock = jest
+          .spyOn(global.console, "error")
+          .mockImplementation();
+        dynamoMock.rejectsOnce("mock error");
+      });
+  
+      afterEach(() => {
+        consoleErrorMock.mockRestore();
+      });
+  
+      test("logs the error message", async () => {
+        await handler(TEST_SQS_EVENT);
+        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
+      });
+  
+      test("sends the event to the dead letter queue", async () => {
+        await handler(TEST_SQS_EVENT);
+        expect(sqsMock.commandCalls(SendMessageCommand).length).toEqual(1);
+      });
+    });
+  });

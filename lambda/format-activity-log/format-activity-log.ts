36b3eed9c28cf529061eb03a7c568ee26abd32cc
Divya <divya.parameswaran@digital.cabinet-office.gov.uk>
2023-05-26 12:28:15 +0100

    GUA:748: Update test for validation and add logic in lambda

--- lambda/format-activity-log/format-activity-log.ts
+++ lambda/format-activity-log/format-activity-log.ts
@@ -1,38 +1,80 @@
-import { SQSEvent } from 'aws-lambda';
-import { Activity, ActivityLogEntry, TxmaEvent, UserActivityLog } from './models';
+import { SQSEvent } from "aws-lambda";
 
-export const handler = async (event: SQSEvent): Promise<void> => {
+import {
+  Activity,
+  ActivityLogEntry,
+  TxmaEvent,
+  UserActivityLog,
+  UserData,
+} from "./models";
 
+export const handler = async (event: SQSEvent): Promise<void> => {};
+
+export const formatIntoActivitLogEntry = (
+  userActivityLog: UserActivityLog
+): ActivityLogEntry => {
+  if (userActivityLog.activityLogEntry == undefined) {
+    return createNewActivityLogEntryFromTxmaEvent(userActivityLog.txmaEvent);
+  } else {
+    userActivityLog.activityLogEntry.activities.push(
+      activityFromTxmaEvent(userActivityLog.txmaEvent)
+    );
+    return userActivityLog.activityLogEntry;
+  }
 };
 
-export const formatIntoActivitLogEntry = (userActivityLog : UserActivityLog ) 
-    : ActivityLogEntry => {
-        if (userActivityLog.activityLogEntry == undefined) {
-            return createNewActivityLogEntryFromTxmaEvent(userActivityLog.txmaEvent)
-        } else {
-            userActivityLog.activityLogEntry.activities.push(activityFromTxmaEvent(userActivityLog.txmaEvent))
-            return userActivityLog.activityLogEntry
-        }
-};
-
-const createNewActivityLogEntryFromTxmaEvent = (txmaEvent: TxmaEvent) : ActivityLogEntry => ({
-    event_type: txmaEvent.event_name,
-    session_id: txmaEvent.user.session_id,
-    user_id: txmaEvent.user.user_id,
-    timestamp: txmaEvent.timestamp,
-    activities: [
-        {
-            type: txmaEvent.event_name,
-            client_id: txmaEvent.client_id,
-            timestamp: txmaEvent.timestamp
-        }
-    ],
-    truncated: false
+const createNewActivityLogEntryFromTxmaEvent = (
+  txmaEvent: TxmaEvent
+): ActivityLogEntry => ({
+  event_type: txmaEvent.event_name,
+  session_id: txmaEvent.user.session_id,
+  user_id: txmaEvent.user.user_id,
+  timestamp: txmaEvent.timestamp,
+  activities: [
+    {
+      type: txmaEvent.event_name,
+      client_id: txmaEvent.client_id,
+      timestamp: txmaEvent.timestamp,
+    },
+  ],
+  truncated: false,
 });
 
 const activityFromTxmaEvent = (txmaEvent: TxmaEvent): Activity => ({
-    type: txmaEvent.event_name,
-    client_id: txmaEvent.client_id,
-    timestamp: txmaEvent.timestamp
+  type: txmaEvent.event_name,
+  client_id: txmaEvent.client_id,
+  timestamp: txmaEvent.timestamp,
 });
 
+export const validateUser = (user: UserData): void => {
+  if (!user.user_id || !user.session_id) {
+    throw new Error(`Could not validate User ${JSON.stringify(user)}`);
+  }
+};
+
+export const validateTxmaEventBody = (txmaEvent: TxmaEvent): void => {
+  if (
+    txmaEvent.timestamp !== undefined &&
+    txmaEvent.event_name !== undefined &&
+    txmaEvent.client_id !== undefined &&
+    txmaEvent.user !== undefined
+  ) {
+    validateUser(txmaEvent.user);
+  } else {
+    throw new Error(
+      `Could not validate UserServices ${JSON.stringify(txmaEvent)}`
+    );
+  }
+};
+
+export const validateUserActivityLog = (
+  userActivityLog: UserActivityLog
+): void => {
+  if (!userActivityLog.txmaEvent) {
+    throw new Error(
+      `Could not validate UserActivityLog ${JSON.stringify(userActivityLog)}`
+    );
+  } else {
+    validateTxmaEventBody(userActivityLog.txmaEvent);
+  }
+};
--- lambda/format-activity-log/tests/format-activity-log.test.ts
+++ lambda/format-activity-log/tests/format-activity-log.test.ts
@@ -1,26 +1,130 @@
-import { formatIntoActivitLogEntry } from "../format-activity-log"
+import {
+  formatIntoActivitLogEntry,
+  validateTxmaEventBody,
+  validateUser,
+  validateUserActivityLog,
+} from "../format-activity-log";
 import { ActivityLogEntry } from "../models";
-import { TEST_ACTIVITY_LOG_ENTRY,
-    TEST_ACTIVITY_LOG_ENTRY_WITH_TWO_ACTIVITIES,
-    TEST_USER_ACTIVITY_LOG_UNDEFINED_LOG_ENTRY,
-    TEST_USER_ACTIVITY_SECOND_TXMA_EVENT,
-    secondEventType } from "./test-helper"
+import {
+  TEST_ACTIVITY_LOG_ENTRY,
+  TEST_ACTIVITY_LOG_ENTRY_WITH_TWO_ACTIVITIES,
+  TEST_USER_ACTIVITY_LOG_UNDEFINED_LOG_ENTRY,
+  TEST_USER_ACTIVITY_SECOND_TXMA_EVENT,
+  secondEventType,
+  sessionId,
+  userId,
+} from "./test-helper";
 
 describe("formatIntoActivitLogEntry", () => {
+  test("txma event with no existing ActivityLogEntry", () => {
+    expect(
+      formatIntoActivitLogEntry(TEST_USER_ACTIVITY_LOG_UNDEFINED_LOG_ENTRY)
+    ).toEqual(TEST_ACTIVITY_LOG_ENTRY);
+  });
 
-    test("txma event with no existing ActivityLogEntry", () => {
-        expect(formatIntoActivitLogEntry(TEST_USER_ACTIVITY_LOG_UNDEFINED_LOG_ENTRY))
-        .toEqual(TEST_ACTIVITY_LOG_ENTRY)
-    });
-
-    test("new txma event and existing activity log entry", () => {
-        const activityLogEntry: ActivityLogEntry = formatIntoActivitLogEntry(TEST_USER_ACTIVITY_SECOND_TXMA_EVENT);
-        expect(activityLogEntry)
-        .toEqual(TEST_ACTIVITY_LOG_ENTRY_WITH_TWO_ACTIVITIES)
-        expect(activityLogEntry.activities[1].type).toEqual(secondEventType)
-    });
-
+  test("new txma event and existing activity log entry", () => {
+    const activityLogEntry: ActivityLogEntry = formatIntoActivitLogEntry(
+      TEST_USER_ACTIVITY_SECOND_TXMA_EVENT
+    );
+    expect(activityLogEntry).toEqual(
+      TEST_ACTIVITY_LOG_ENTRY_WITH_TWO_ACTIVITIES
+    );
+    expect(activityLogEntry.activities[1].type).toEqual(secondEventType);
+  });
 });
 
+describe("validatUserActivityLog", () => {
+  test("throws error when txmaEvent is missing", () => {
+    const userActivityLog = JSON.parse(JSON.stringify(JSON.stringify({})));
+    expect(() => {
+      validateUserActivityLog(userActivityLog);
+    }).toThrowError();
+  });
+});
+
+describe("validateTxmaEventBody", () => {
+  test("doesn't throw an error with valid txma data", () => {
+    expect(
+      validateTxmaEventBody(
+        TEST_USER_ACTIVITY_LOG_UNDEFINED_LOG_ENTRY.txmaEvent
+      )
+    ).toBe(undefined);
+  });
+
+  test("throws error when client_id is missing", () => {
+    const invalidTxmaEvent = {
+      ...TEST_USER_ACTIVITY_LOG_UNDEFINED_LOG_ENTRY.txmaEvent,
+      client_id: undefined,
+    };
+    const txmaEvent = JSON.parse(JSON.stringify(invalidTxmaEvent));
+    expect(() => {
+      validateTxmaEventBody(txmaEvent);
+    }).toThrowError();
+  });
+
+  test("throws error when timestamp is missing", () => {
+    const invalidTxmaEvent = {
+      ...TEST_USER_ACTIVITY_LOG_UNDEFINED_LOG_ENTRY.txmaEvent,
+      timestamp: undefined,
+    };
+    const txmaEvent = JSON.parse(JSON.stringify(invalidTxmaEvent));
+    expect(() => {
+      validateTxmaEventBody(txmaEvent);
+    }).toThrowError();
+  });
+
+  test("throws error when event name is missing", () => {
+    const invalidTxmaEvent = {
+      ...TEST_USER_ACTIVITY_LOG_UNDEFINED_LOG_ENTRY.txmaEvent,
+      event_name: undefined,
+    };
+    const txmaEvent = JSON.parse(JSON.stringify(invalidTxmaEvent));
+    expect(() => {
+      validateTxmaEventBody(txmaEvent);
+    }).toThrowError();
+  });
+
+  test(" throws error when user is missing", () => {
+    const invalidTxmaEvent = {
+      ...TEST_USER_ACTIVITY_LOG_UNDEFINED_LOG_ENTRY.txmaEvent,
+      user: undefined,
+    };
+    const txmaEvent = JSON.parse(JSON.stringify(invalidTxmaEvent));
+    expect(() => {
+      validateTxmaEventBody(txmaEvent);
+    }).toThrowError();
+  });
+
+  test("throws error when user_id  is missing", () => {
+    const invalidTxmaEvent = {
+      ...TEST_USER_ACTIVITY_LOG_UNDEFINED_LOG_ENTRY.txmaEvent,
+      user: { session_id: sessionId },
+    };
+    const txmaEvent = JSON.parse(JSON.stringify(invalidTxmaEvent));
+    expect(() => {
+      validateTxmaEventBody(txmaEvent);
+    }).toThrowError();
+  });
+
+  test("throws error when session_id  is missing", () => {
+    const invalidTxmaEvent = {
+      ...TEST_USER_ACTIVITY_LOG_UNDEFINED_LOG_ENTRY.txmaEvent,
+      user: { user_id: userId },
+    };
+    const txmaEvent = JSON.parse(JSON.stringify(invalidTxmaEvent));
+    expect(() => {
+      validateTxmaEventBody(txmaEvent);
+    }).toThrowError();
+  });
+});
+
+describe("validateUser", () => {
+  test("throws error when user is is missing", () => {
+    const inValidUser = JSON.parse(JSON.stringify({}));
+    expect(() => {
+      validateUser(inValidUser);
+    }).toThrowError();
+  });
+});
 // should we order Activities by order recieved or chronologically earliest time recieved ?
 // should we update top level timestamp to match earlieast timestamp? (not just set as first event)

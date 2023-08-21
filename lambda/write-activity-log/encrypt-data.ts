d8c762a83ea41cc6abef0264c1188e0651827c89
Dougal Rees <24165331+DougsCER@users.noreply.github.com>
2023-08-21 17:11:11 +0100

    sideways stepping

--- lambda/write-activity-log/encrypt-data.ts
+++ lambda/write-activity-log/encrypt-data.ts
@@ -1,46 +1,47 @@
-import { KmsKeyringNode, buildEncrypt } from '@aws-crypto/client-node';
-import { getHashedAccessCheckValue } from '../commons/get-hashed-access-check-value';
-import { AppConfigService } from './app-config-service';
-import { ENCODING, MetricNames } from '../data-types/constants';
-import logger from '../commons/logger';
+import { KmsKeyringNode, buildEncrypt } from "@aws-crypto/client-node";
+import { buildKmsKeyring } from "./kms-keyring-builder";
+import { getHashedAccessCheckValue } from "./get-access-check-value";
 
-import { logAndPublishMetric } from '../commons/metrics';
-import { buildKmsKeyring } from './kms-keyring-builder';
-
-const MAX_ENCRYPTED_DATA_KEY : number = Number(String(process.env.MAX_ENCRYPTED_DATA_KEY));
+const MAX_ENCRYPTED_DATA_KEY: number = Number(
+  String(process.env.MAX_ENCRYPTED_DATA_KEY)
+);
 const { GENERATOR_KEY_ARN } = process.env;
 const { VERIFY_ACCESS_PARAM_NAME } = process.env;
 const { AWS_REGION } = process.env;
 const { ACCOUNT_ID } = process.env;
 const { ENVIRONMENT } = process.env;
 
+const ENCODING = "base64";
+
 let kmsKeyring: KmsKeyringNode;
 const encryptClientConfig = { maxEncryptedDataKeys: MAX_ENCRYPTED_DATA_KEY };
 let encryptClient = buildEncrypt(encryptClientConfig);
 
-
-export async function encryptData(dataToEncrypt: string, userId: string): Promise<string> {
-
-  checkEnvironmentVariableExists(GENERATOR_KEY_ARN,VERIFY_ACCESS_PARAM_NAME, AWS_REGION, ACCOUNT_ID, ENVIRONMENT);
-
+export async function encryptData(
+  dataToEncrypt: string,
+  userId: string
+): Promise<string> {
   kmsKeyring ??= await buildKmsKeyring(GENERATOR_KEY_ARN);
   encryptClient ??= buildEncrypt(encryptClientConfig);
   const { encrypt } = encryptClient;
 
-  let accessCheckValue;
-  try {
-    accessCheckValue = await getHashedAccessCheckValue(VERIFY_ACCESS_PARAM_NAME);
-  } catch (error) {
-    logger.error('Unable to obtain Access Verification value.');
-    throw error;
-  }
+  if (
+    VERIFY_ACCESS_PARAM_NAME !== undefined &&
+    AWS_REGION !== undefined &&
+    ACCOUNT_ID !== undefined &&
+    ENVIRONMENT !== undefined
+  ) {
+    let accessCheckValue;
+    try {
+      accessCheckValue = await getHashedAccessCheckValue(
+        VERIFY_ACCESS_PARAM_NAME
+      );
+    } catch (error) {
+      console.error("Unable to obtain Access Verification value.");
+      throw error;
+    }
 
-  // if (
-  //   AWS_REGION !== undefined &&
-  //   ACCOUNT_ID !== undefined &&
-  //   ENVIRONMENT !== undefined
-  // ) {
-    const encryptionContext: EncryptionContext = {
+    const encryptionContext = {
       origin: AWS_REGION,
       accountId: ACCOUNT_ID,
       stage: ENVIRONMENT,
@@ -49,33 +50,15 @@ export async function encryptData(dataToEncrypt: string, userId: string): Promis
     };
 
     try {
-      const { result } = await encrypt(kmsKeyring, dataToEncrypt, {encryptionContext});
+      const { result } = await encrypt(kmsKeyring, dataToEncrypt, {
+        encryptionContext,
+      });
       return result.toString(ENCODING);
     } catch (error: unknown) {
-      logger.error('Failed to encrypt data.', { error });
-      logAndPublishMetric(MetricNames.ENCRYPTION_FAILED);
+      console.error("Failed to encrypt data.", { error });
       throw error;
     }
   }
 
-  
-// }
-
-export function checkEnvironmentVariableExists(...valuesToCheck: Array<(string | undefined )>) {
-  const notPresentValues = valuesToCheck.filter(function (value) {
-    return value === "undefined";
-  })
-  if (notPresentValues.length > 0) {
-    throw new Error(
-      `an environment variable is not present`
-    )    
-  }
-}
-
-interface EncryptionContext {
-  origin: string;
-  accountId: string;
-  stage: string;
-  userId: string;
-  accessCheckValue: string;
+  throw new Error(`an environment variable is not present`);
 }

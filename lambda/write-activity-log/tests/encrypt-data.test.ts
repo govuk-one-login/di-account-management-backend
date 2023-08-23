8ff31e2c52a622a0de23887847c2d89abaf4940a
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2023-08-23 09:26:15 +0100

    BAU - linting

--- lambda/write-activity-log/tests/encrypt-data.test.ts
+++ lambda/write-activity-log/tests/encrypt-data.test.ts
@@ -1,57 +1,67 @@
-import { mockClient } from 'aws-sdk-client-mock';
-import { GetSecretValueCommand, SecretsManagerClient } from '@aws-sdk/client-secrets-manager';
-import { buildEncrypt, MessageHeader } from '@aws-crypto/client-node';
-import { when } from 'jest-when';
-import { encryptData } from '../encrypt-data';
-import { TEST_ACTIVITY_LOG_ENTRY } from './test-helpers';
+import { mockClient } from "aws-sdk-client-mock";
+import {
+  GetSecretValueCommand,
+  SecretsManagerClient,
+} from "@aws-sdk/client-secrets-manager";
+import { buildEncrypt, MessageHeader } from "@aws-crypto/client-node";
+import { when } from "jest-when";
+import { encryptData } from "../encrypt-data";
+import { TEST_ACTIVITY_LOG_ENTRY } from "./test-helpers";
 
-const mockedSecretsManager = mockClient(SecretsManagerClient).on(GetSecretValueCommand);
+const mockedSecretsManager = mockClient(SecretsManagerClient).on(
+  GetSecretValueCommand
+);
 
-describe('encryptData', () => {
+describe("encryptData", () => {
   let encryptDataInput: any;
   beforeEach(() => {
-
     const activityLogEntry = JSON.stringify(TEST_ACTIVITY_LOG_ENTRY);
 
     encryptDataInput = {
       activityLogData: activityLogEntry,
-      userId: 'test_user_123',
+      userId: "test_user_123",
     };
 
-    process.env.GENERATOR_KEY_ARN = "arn:aws:kms:eu-west-2:111122223333:key/bc436485-5092-42b8-92a3-0aa8b93536dc";
+    process.env.GENERATOR_KEY_ARN =
+      "arn:aws:kms:eu-west-2:111122223333:key/bc436485-5092-42b8-92a3-0aa8b93536dc";
     process.env.VERIFY_ACCESS_PARAM_NAME = "access-param-test-value";
     process.env.ACCOUNT_ID = "12345";
     process.env.AWS_REGION = "GLOBAL";
     process.env.ENVIRONMENT = "dev";
     process.env.WRAPPING_KEY_ARN = "wrapping-key";
     process.env.BACKUP_WRAPPER_KEY_ARN = "backup-wrapping-key";
-
   });
 
-  it('should return the encryption result Base64 encoded', async () => {
+  it("should return the encryption result Base64 encoded", async () => {
     mockedSecretsManager.resolves({
-      SecretString: 'testSecretValue',
+      SecretString: "testSecretValue",
     });
     when(buildEncrypt().encrypt).mockResolvedValue({
-      result: Buffer.from('testEncryptedDataString'),
+      result: Buffer.from("testEncryptedDataString"),
       messageHeader: {} as MessageHeader,
     });
-    const result = await encryptData(encryptDataInput.activityLogData, encryptDataInput.userId);
-    expect(result).toEqual('dGVzdEVuY3J5cHRlZERhdGFTdHJpbmc='); // expect fail
+    const result = await encryptData(
+      encryptDataInput.activityLogData,
+      encryptDataInput.userId
+    );
+    expect(result).toEqual("dGVzdEVuY3J5cHRlZERhdGFTdHJpbmc="); // expect fail
     expect(buildEncrypt).toHaveBeenCalled();
     expect(buildEncrypt().encrypt).toHaveBeenCalledWith(
-      { generatorKeyId: 'arn:aws:kms:eu-west-2:111122223333:key/bc436485-5092-42b8-92a3-0aa8b93536dc' },  // incorrect
-      'eyJhbGciOiJIUzI1NiIs9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6jM5M.SflKxwRJSMeKKF2QT4f_adQssw5c',
+      {
+        generatorKeyId:
+          "arn:aws:kms:eu-west-2:111122223333:key/bc436485-5092-42b8-92a3-0aa8b93536dc",
+      }, // incorrect
+      "eyJhbGciOiJIUzI1NiIs9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6jM5M.SflKxwRJSMeKKF2QT4f_adQssw5c",
       {
         encryptionContext: {
-          accountId: process.env['ACCOUNT_ID'],
-          origin: process.env['AWS_REGION'],
-          accessCheckValue: 'a393a60804eeb859d0f2d938c530b352b826583b9d648b95a8acdc6e75da85e4',
-          stage: process.env['ENVIRONMENT'],
-          userId: 'test_user_123',
+          accountId: process.env.ACCOUNT_ID,
+          origin: process.env.AWS_REGION,
+          accessCheckValue:
+            "a393a60804eeb859d0f2d938c530b352b826583b9d648b95a8acdc6e75da85e4",
+          stage: process.env.ENVIRONMENT,
+          userId: "test_user_123",
         },
-      },
+      }
     );
   });
-
 });

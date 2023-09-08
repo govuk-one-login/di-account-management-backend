be44ecfffed6ba206214abaef0f9b31d353d3e60
Dougal Rees <24165331+DougsCER@users.noreply.github.com>
2023-09-08 14:58:51 +0100

    remove debug logging

--- lambda/write-activity-log/encrypt-data.ts
+++ lambda/write-activity-log/encrypt-data.ts
@@ -13,9 +13,6 @@ const encryptData = async (
   toEncrypt: string,
   userId: string
 ): Promise<string> => {
-  console.log(
-    `encrypting string with user_id: ${userId} and string to encrypt: ${toEncrypt}`
-  );
   const { GENERATOR_KEY_ARN } = process.env;
   const { WRAPPING_KEY_ARN } = process.env;
   const { BACKUP_WRAPPING_KEY_ARN } = process.env;
@@ -29,12 +26,6 @@ const encryptData = async (
     WRAPPING_KEY_ARN,
     BACKUP_WRAPPING_KEY_ARN
   );
-
-  console.log(
-    `encrypting string with user_id: ${userId} kms keyring: ${JSON.stringify(
-      kmsKeyring
-    )}`
-  );
   encryptClient ??= buildEncrypt(encryptClientConfig);
   const { encrypt } = encryptClient;
 
@@ -47,16 +38,10 @@ const encryptData = async (
     let accessCheckValue;
     try {
       accessCheckValue = await getHashedAccessCheckValue(VERIFY_ACCESS_VALUE);
-      console.log(
-        `encrypting string with user_id: ${userId} got hashed access check value: ${accessCheckValue}`
-      );
     } catch (error) {
       console.error("Unable to obtain Access Verification value.");
       throw error;
     }
-    console.log(
-      `encrypting string with user_id: ${userId} about to construct encryption context`
-    );
     const encryptionContext = {
       origin: AWS_REGION,
       accountId: ACCOUNT_ID,
@@ -64,26 +49,17 @@ const encryptData = async (
       userId,
       accessCheckValue,
     };
-    console.log(
-      `encrypting string with user_id: ${userId} constructing encryption context: ${JSON.stringify(
-        encryptionContext
-      )}`
-    );
     try {
       const { result } = await encrypt(kmsKeyring, toEncrypt, {
         encryptionContext,
       });
       const response = result.toString(ENCODING);
-      console.log(
-        `encrypting string with user_id: ${userId} returning encrypted string ${response}`
-      );
       return response;
     } catch (error: unknown) {
       console.error("Failed to encrypt data.", { error });
       throw error;
     }
   }
-
   throw new Error(`an environment variable is not present`);
 };
 

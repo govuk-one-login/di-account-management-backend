82ebaa67140d1ef9e47729b5b12eac4080c17769
Dougal Rees <24165331+DougsCER@users.noreply.github.com>
2023-09-08 14:45:46 +0100

    formatting

--- lambda/write-activity-log/get-access-check-value.ts
+++ lambda/write-activity-log/get-access-check-value.ts
@@ -1,17 +1,8 @@
 import crypto from "node:crypto";
-import { getSecret } from "@aws-lambda-powertools/parameters/secrets";
 
 const SecretValueAlgorithm = "sha256";
 const SecretValueEncoding = "hex";
 
-/**
- * A function for retrieving a secret string value from Secrets Manager.
- *
- * @param secretId - the name of the secret to retrieve
- * @returns the hexadecimal representation of the hashed secret value on promise fulfillment
- * @throws {@link TypeError}
- * Thrown when the secret value is falsy
- */
 const getHashedAccessCheckValue = async (
   accessCheckValue: string
 ): Promise<string> => {
--- lambda/write-activity-log/tests/kms-keyring-builder.test.ts
+++ lambda/write-activity-log/tests/kms-keyring-builder.test.ts
@@ -1,40 +1,53 @@
-import { KmsKeyringNode } from "@aws-crypto/client-node"
-import buildKmsKeyring from "../kms-keyring-builder"
+import { KmsKeyringNode } from "@aws-crypto/client-node";
+import buildKmsKeyring from "../kms-keyring-builder";
 
-const exampleArn: string = "arn:aws:kms:eu-west-2:111122223333:key/bc436485-5092-42b8-92a3-0aa8b93536dc";
+const exampleArn: string =
+  "arn:aws:kms:eu-west-2:111122223333:key/bc436485-5092-42b8-92a3-0aa8b93536dc";
 
 describe.only("kmsKeyringBuilder", () => {
   test("construct KmsKeyring corretly", async () => {
     const kmsKeyring: KmsKeyringNode = await buildKmsKeyring(
-      exampleArn, exampleArn, exampleArn
-    )
+      exampleArn,
+      exampleArn,
+      exampleArn
+    );
     expect(kmsKeyring.keyIds).toHaveLength(2);
     expect(kmsKeyring.generatorKeyId).toEqual(exampleArn);
     expect(kmsKeyring.keyIds[0]).toEqual(exampleArn);
-  })
-
+  });
 
   test("throws error when generator key is not valid ARN", async () => {
     await expect(async () => {
-      const kmsKeyring: KmsKeyringNode = await buildKmsKeyring(
-        "not-valid-arn", exampleArn
-      )
-    }).rejects.toThrowError("ARN for envelope encryption Generator key is invalid.");
-  })
+      await buildKmsKeyring(
+        "not-valid-arn",
+        exampleArn
+      );
+    }).rejects.toThrowError(
+      "ARN for envelope encryption Generator key is invalid."
+    );
+  });
 
   test("throws error when wrapper key is not valid ARN", async () => {
     await expect(async () => {
-      const kmsKeyring: KmsKeyringNode = await buildKmsKeyring(
-        exampleArn, "not-valid-arn", exampleArn
-      )
-    }).rejects.toThrowError("ARN for main envelope encryption wrapping key is invalid.");
-  })
+      await buildKmsKeyring(
+        exampleArn,
+        "not-valid-arn",
+        exampleArn
+      );
+    }).rejects.toThrowError(
+      "ARN for main envelope encryption wrapping key is invalid."
+    );
+  });
 
   test("throws error when wrapper key is not defined", async () => {
     await expect(async () => {
-      const kmsKeyring: KmsKeyringNode = await buildKmsKeyring(
-        exampleArn, undefined, exampleArn
-      )
-    }).rejects.toThrowError("ARN for main envelope encryption wrapping key is undefined.");
-  })
-})
+      await buildKmsKeyring(
+        exampleArn,
+        undefined,
+        exampleArn
+      );
+    }).rejects.toThrowError(
+      "ARN for main envelope encryption wrapping key is undefined."
+    );
+  });
+});

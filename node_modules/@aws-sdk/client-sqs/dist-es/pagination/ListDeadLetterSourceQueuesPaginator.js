a0fed1b8e5ab8729db42396ece225423f0aa1500
Andrew Moores <andrew.moores@digital.cabinet-office.gov.uk>
2024-01-29 15:50:04 +0000

    OLH-1111 removing node_modules.

--- node_modules/@aws-sdk/client-sqs/dist-es/pagination/ListDeadLetterSourceQueuesPaginator.js
+++
@@ -1,4 +0,0 @@
-import { createPaginator } from "@smithy/core";
-import { ListDeadLetterSourceQueuesCommand, } from "../commands/ListDeadLetterSourceQueuesCommand";
-import { SQSClient } from "../SQSClient";
-export const paginateListDeadLetterSourceQueues = createPaginator(SQSClient, ListDeadLetterSourceQueuesCommand, "NextToken", "NextToken", "MaxResults");
--- node_modules/@aws-sdk/client-sqs/dist-es/pagination/ListQueuesPaginator.js
+++
@@ -1,4 +0,0 @@
-import { createPaginator } from "@smithy/core";
-import { ListQueuesCommand } from "../commands/ListQueuesCommand";
-import { SQSClient } from "../SQSClient";
-export const paginateListQueues = createPaginator(SQSClient, ListQueuesCommand, "NextToken", "NextToken", "MaxResults");

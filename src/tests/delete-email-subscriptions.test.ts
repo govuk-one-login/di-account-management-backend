62110773cd6baed99c8c874e3979b1f9a8354a82
Andrew Moores <andrew.moores@digital.cabinet-office.gov.uk>
2024-01-29 12:03:36 +0000

    OLH-1111 adding tests.

---
+++ src/tests/delete-email-subscriptions.test.ts
@@ -0,0 +1,117 @@
+import {
+  handler,
+  getRequestConfig,
+  validateUserData,
+} from "../delete-email-subscriptions";
+import type { SNSEvent, SNSMessage, SNSEventRecord } from "aws-lambda";
+
+export const TEST_USER_DATA = {
+  user_id: "user-id",
+  access_token: "access_token",
+  email: "email",
+  source_ip: "source_ip",
+  persistent_session_id: "persistent_session_id",
+  session_id: "session_id",
+  public_subject_id: "public_subject_id",
+  legacy_subject_id: "legacy_subject_id",
+};
+
+const TEST_SNS_MESSAGE: SNSMessage = {
+  SignatureVersion: "SignatureVersion",
+  Timestamp: "Timestamp",
+  Signature: "Signature",
+  SigningCertUrl: "SigningCertUrl",
+  MessageId: "MessageId",
+  Message: JSON.stringify(TEST_USER_DATA),
+  MessageAttributes: {},
+  Type: "Type",
+  UnsubscribeUrl: "unsubscribeUrl",
+  TopicArn: "TopicArn",
+  Subject: "Subject",
+};
+
+const TEST_SNS_EVENT_RECORD: SNSEventRecord = {
+  EventVersion: "1",
+  EventSubscriptionArn: "arn",
+  EventSource: "source",
+  Sns: TEST_SNS_MESSAGE,
+};
+
+export const TEST_SNS_EVENT: SNSEvent = {
+  Records: [TEST_SNS_EVENT_RECORD],
+};
+
+describe("handler", () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("that it successfully processes the SNS message", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires,global-require
+    const module = require("../delete-email-subscriptions");
+    const validateUserDataMock = jest
+      .spyOn(module, "validateUserData")
+      .mockReturnValueOnce("validateUserData-mock");
+
+    const deleteEmailSubscriptionMock = jest
+      .spyOn(module, "deleteEmailSubscription")
+      .mockReturnValue("deleteEmailSubscription-mock");
+
+    await expect(handler(TEST_SNS_EVENT)).resolves.not.toThrowError();
+    expect(validateUserDataMock).toHaveBeenCalledTimes(1);
+    expect(validateUserDataMock).toHaveBeenCalledWith(TEST_USER_DATA);
+    expect(deleteEmailSubscriptionMock).toHaveBeenCalledTimes(1);
+    expect(deleteEmailSubscriptionMock).toHaveBeenCalledWith(TEST_USER_DATA);
+  });
+});
+
+describe("getRequestConfig", () => {
+  test("that it returns the request config in the correct format", () => {
+    expect(getRequestConfig("TOKEN")).toEqual({
+      headers: { Authorization: "Bearer TOKEN" },
+      method: "DELETE",
+    });
+  });
+});
+
+describe("validateUserData", () => {
+  test("that it does not throw an error when the SNS message is valid", () => {
+    expect(validateUserData(TEST_USER_DATA)).toBe(TEST_USER_DATA);
+  });
+
+  test("that it throws an error if the SNS message is missing the required attribute public_subject_id", () => {
+    const userData = JSON.parse(
+      JSON.stringify({
+        user_id: "user-id",
+        access_token: "access_token",
+        email: "email",
+        source_ip: "source_ip",
+        persistent_session_id: "persistent_session_id",
+        session_id: "session_id",
+        legacy_subject_id: "legacy_subject_id",
+      })
+    );
+    expect(() => {
+      validateUserData(userData);
+    }).toThrowError(`userData is not valid`);
+  });
+
+  test("that it does not throw an error if the SNS message is missing the non-required attribute legacy_subject_id", () => {
+    const userData = JSON.parse(
+      JSON.stringify({
+        user_id: "user-id",
+        access_token: "access_token",
+        email: "email",
+        source_ip: "source_ip",
+        persistent_session_id: "persistent_session_id",
+        session_id: "session_id",
+        public_subject_id: "public_subject_id",
+      })
+    );
+    expect(validateUserData(userData)).toBe(userData);
+  });
+});

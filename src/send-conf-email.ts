d65effdb9515f454e0134fbdfc4a2c9edab75678
Saral Kaushik <saral.kaushik@digital.cabinet-office.gov.uk>
2024-02-19 14:44:31 +0000

    add debugging

--- src/send-conf-email.ts
+++ src/send-conf-email.ts
@@ -2,7 +2,8 @@ import { SNSEvent } from "aws-lambda";
 import { sendSqsMessage } from "./common/sqs";
 import { SuspiciousActivityEvent } from "./common/model";
 import assert from "node:assert/strict";
-import { NotifyClient } from "notifications-node-client";
+import axios from "axios";
+import "axios-debug-log";
 
 export const validateSuspiciousActivity = (
   suspiciousActivityEvent: SuspiciousActivityEvent
@@ -11,17 +12,22 @@ export const validateSuspiciousActivity = (
 };
 
 export const sendConfMail = async (email: string) => {
-  console.log("sending email");
-  const client = new NotifyClient(
-    "test-4237628b-1a8d-457a-89c4-8b136c18b7d7-a998abca-4854-408d-930d-a82189fc7459"
-  );
-  console.log("created client");
-  return client.sendEmail("4e07abfb-18cf-49d9-a697-c1e53dc2da6f", email, {
-    personalisation: {
-      name: "Tom",
-    },
-    reference: "abc",
+  const instance = axios.create({
+    baseURL: "https://api.notifications.service.gov.uk",
   });
+
+  return await instance.post("/test", {});
+  //console.log("sending email");
+  //const client = new NotifyClient(
+  //  "test-4237628b-1a8d-457a-89c4-8b136c18b7d7-a998abca-4854-408d-930d-a82189fc7459"
+  //);
+  //console.log("created client");
+  //return client.sendEmail("4e07abfb-18cf-49d9-a697-c1e53dc2da6f", email, {
+  //  personalisation: {
+  //    name: "Tom",
+  //  },
+  //  reference: "abc",
+  //});
 };
 
 export const handler = async (event: SNSEvent): Promise<void> => {

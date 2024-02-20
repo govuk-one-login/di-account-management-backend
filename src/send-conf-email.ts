fc3a5a768f16fda61db27ded15657d9a702bd7f5
Saral Kaushik <saral.kaushik@digital.cabinet-office.gov.uk>
2024-02-20 14:07:24 +0000

    add welsh

--- src/send-conf-email.ts
+++ src/send-conf-email.ts
@@ -5,22 +5,62 @@ import assert from "node:assert/strict";
 import { NotifyClient } from "notifications-node-client";
 import "axios-debug-log";
 import { getSecret } from "@aws-lambda-powertools/parameters/secrets";
-import clientRegistry from "./config/clientRegistry.json";
+import clientRegistryEn from "./config/clientRegistry.en.json";
+import clientRegistryCy from "./config/clientRegistry.cy.json";
 
 type Client = {
   header: string;
-  description: string;
+  description?: string;
   link_text: string;
   link_href: string;
+};
+
+type ClientRegistryEnvironmentsEnum =
+  | "production"
+  | "integration"
+  | "staging"
+  | "build"
+  | "dev"
+  | "local";
+
+type ClientRegistryEnvronment = Record<string, Client>;
+
+type ClientRegistry = Record<
+  ClientRegistryEnvironmentsEnum,
+  ClientRegistryEnvronment
+>;
+
+export const getClientInfo = (
+  clientRegistry: ClientRegistry,
+  environment: ClientRegistryEnvironmentsEnum,
+  id: string
+) => {
+  assert(
+    clientRegistry[environment],
+    `${environment} does not exist in client registry`
+  );
+
+  const registry = clientRegistry[environment];
+
+  assert(
+    registry[id],
+    `${id} does not exist in ${environment} client registry]`
+  );
+
+  const client: Client = registry[id];
+
+  return client;
 };
 
 export const formatActivityObjectForEmail = (
   activity: SuspiciousActivityEvent
 ): {
   email: string;
-  clientName: string;
+  clientNameEn: string;
+  clientNameCy: string;
 } => {
-  const envName = process.env.ENVIRONMENT_NAME as keyof typeof clientRegistry;
+  const envName = process.env
+    .ENVIRONMENT_NAME as ClientRegistryEnvironmentsEnum;
   assert(envName, "ENVIRONMENT_NAME env variable not set");
 
   assert(
@@ -28,24 +68,13 @@ export const formatActivityObjectForEmail = (
     "Email address not present in Suspicious Activity Event"
   );
 
-  assert(
-    clientRegistry[envName],
-    `${envName} does not exist in config/clientRegistry.json`
-  );
-
-  const registry = clientRegistry[envName];
-  const clientId = activity.client_id as keyof typeof registry;
-
-  assert(
-    registry[clientId],
-    `${activity.client_id} does not exist in config/clientRegistry.json[${envName}]`
-  );
-
-  const client: Client = registry[clientId];
+  const clientEn = getClientInfo(clientRegistryEn, envName, activity.client_id);
+  const clientCy = getClientInfo(clientRegistryCy, envName, activity.client_id);
 
   return {
     email: activity.user.email,
-    clientName: client.header,
+    clientNameEn: clientEn.header,
+    clientNameCy: clientCy.header,
   };
 };
 
@@ -55,10 +84,13 @@ export const sendConfMail = async (
   activity: SuspiciousActivityEvent
 ) => {
   const notifyClient = new NotifyClient(apiKey);
-  const { email, clientName } = formatActivityObjectForEmail(activity);
+  const { email, clientNameEn, clientNameCy } =
+    formatActivityObjectForEmail(activity);
+
   return notifyClient.sendEmail(templateId, email, {
     personalisation: {
-      clientName,
+      clientNameEn,
+      clientNameCy,
     },
     reference: activity.event_id,
   });
@@ -70,9 +102,9 @@ export const handler = async (event: SNSEvent): Promise<void> => {
   await Promise.all(
     event.Records.map(async (record) => {
       try {
-        assert(DLQ_URL, "DLQ_URL env variable not provided");
-        assert(NOTIFY_API_KEY, "NOTIFY_API_KEY env variable not provided");
-        assert(TEMPLATE_ID, "TEMPLATE_ID env variable not provided");
+        assert(DLQ_URL, "DLQ_URL env variable not set");
+        assert(NOTIFY_API_KEY, "NOTIFY_API_KEY env variable not set");
+        assert(TEMPLATE_ID, "TEMPLATE_ID env variable not set");
 
         const notifyApiKey = await getSecret(NOTIFY_API_KEY, {
           maxAge: 900,

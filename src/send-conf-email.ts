d095c961f53d50dbd08b43e2b3e4472ac10e92d5
Peter Fajemisin <peter.fajemisin@digital.cabinet-office.gov.uk>
2024-08-12 13:11:32 +0100

    For debuggin.

--- src/send-conf-email.ts
+++ src/send-conf-email.ts
@@ -171,5 +171,7 @@ const notifyErrorHandler = (err: IError, context: string): never => {
       throw new Error(`Error ${context}: ${JSON.stringify(sanitisedError)}`);
     }
   }
-  throw new Error(`Error ${context}`);
+  throw new Error(
+    `Error context is:${context}, response is: ${JSON.stringify(err.response)}`
+  );
 };

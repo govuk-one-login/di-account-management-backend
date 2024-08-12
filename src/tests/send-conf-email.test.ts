ac696ef4bb7af4a0e998023e7233c84853f2a4e1
Peter Fajemisin <peter.fajemisin@digital.cabinet-office.gov.uk>
2024-08-12 13:18:36 +0100

    For debuggin.

--- src/tests/send-conf-email.test.ts
+++ src/tests/send-conf-email.test.ts
@@ -290,7 +290,7 @@ describe("handler", () => {
       .mockRejectedValueOnce(errorResponse);
 
     await expect(handler(input)).rejects.toThrow(
-      "Error sending email for event"
+      'Error context is:sending email for event, response is: {"status":"404","statusText":"Not Found"}'
     );
   });
 });

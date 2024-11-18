5478f900f87bd05996cd9e3c6907ab13f38b8b15
Peter Fajemisin <peter.fajemisin@digital.cabinet-office.gov.uk>
2024-11-18 17:14:34 +0000

    Attempt to send to queue.

--- pre-merge-integration-tests/api_call.py
+++ pre-merge-integration-tests/api_call.py
@@ -3,15 +3,35 @@ import time
 
 # Initialize a session using CloudFormation
 client = boto3.client('cloudformation')
+# Create SQS client
+sqs = boto3.client('sqs')
 
 # Name of the CloudFormation stack
 stack_name = 'home-backend'  # Replace with actual stack name
+queue_url = "https://sqs.<region>.amazonaws.com/<account_id>/<queue_name>"
+message_body = "{ \"event_name\" : { \"S\" : \"AUTH_AUTH_CODE_ISSUED\" }, \"event_id\" : { \"S\" : \"65093b9c-728d-4c7f-aad2-7e5892a30be0\" }, \"user\" : { \"M\" : { \"user_id\" : { \"S\" : \"user_id\" }, \"session_id\" : { \"S\" : \"7340477f-74da-46d4-9400-d22ae518da3a\" } } }, \"client_id\" : { \"S\" : \"vehicleOperatorLicense\" }, \"timestamp\" : { \"N\" : \"1730800548523\" } }"
+
+def send_message_to_queue(queue_url, message_body, message_attributes=None):
+
+    print(f"Message body is: {message_body}")
+
+    try:
+        # Send message to SQS queue
+        response = sqs.send_message(
+            QueueUrl=queue_url,
+            MessageBody=message_body,
+            MessageAttributes=message_attributes or {}
+        )
+
+        print("Message sent! Message ID:", response['MessageId'])
+    except Exception as e:
+        print("Error sending message:", str(e))
+
 
 def call_describe_stack_events(stack_name):
     print("Describing call stack event")
     try:
         response = client.describe_stack_events(StackName=stack_name)
-        print(f"Stack event response is: {response}")
         return response
     except Exception as e:
         print(f"Error fetching stack events: {e}")
@@ -65,6 +85,21 @@ def wait_for_stack_status(stack_name, max_attempts=10):
     
     print("Max attempts reached or desired status not found within the attempts limit.")
 
+def main(args):
+    if len(args) < 2:
+        print("Usage: api_call.py <param1>")
+        return
+
+    queue_url = args[1]  # First parameter
+
+    print(f"Queue URL is: {queue_url}")
+    send_message_to_queue(queue_url, message_body)
+
 if __name__ == "__main__":
     wait_for_stack_status(stack_name)
+    main(sys.argv)
+    # wait for 500ms,
+    # attempt to get activity log from table with id
+    # retry for up to 10 times before giving up, throw an error is no activity log
+    #otherwise report success and continue
     print("Script execution completed.")

9a0e1cef4fbfadbd2750cadac0736b6fd8b18916
Peter Fajemisin <peter.fajemisin@digital.cabinet-office.gov.uk>
2024-11-18 14:11:04 +0000

    Ue correct stack name.

--- pre-merge-integration-tests/api_call.py
+++ pre-merge-integration-tests/api_call.py
@@ -5,7 +5,7 @@ import time
 client = boto3.client('cloudformation')
 
 # Name of the CloudFormation stack
-stack_name = 'account-mgmt-backend'  # Replace with actual stack name
+stack_name = 'home-backend'  # Replace with actual stack name
 
 def call_describe_stack_events(stack_name):
     print("Describing call stack event")

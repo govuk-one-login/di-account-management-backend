12605de550c6be0d897d564b5e57a61e7bb53713
Peter Fajemisin <peter.fajemisin@digital.cabinet-office.gov.uk>
2024-11-20 08:56:36 +0000

    Waiting for stack to complete update before testing.

--- pre-merge-integration-tests/api_call.py
+++ pre-merge-integration-tests/api_call.py
@@ -18,6 +18,66 @@ message_body = ("{ \"event_name\" : \"AUTH_AUTH_CODE_ISSUED\", \"event_id\" : "
 queue_name = os.getenv('SQS_QUEUE_ARN')
 
 
+def call_describe_stack_events():
+    print("Describing call stack event")
+    try:
+        response = client.describe_stack_events(StackName=stack_name)
+        return response
+    except Exception as e:
+        print(f"Error fetching stack events: {e}")
+        return None
+
+
+def check_stack_status(events):
+    print("Checking stack status")
+    create_in_progress = False
+    update_complete = False
+    create_complete = False
+
+    for event in events:
+        resource_status = event['ResourceStatus']
+        if resource_status == 'CREATE_IN_PROGRESS':
+            create_in_progress = True
+        if resource_status == 'UPDATE_COMPLETE':
+            update_complete = True
+        if resource_status == 'CREATE_COMPLETE':
+            create_complete = True
+
+    return create_in_progress, update_complete, create_complete
+
+
+def wait_for_stack_status(max_attempts=10):
+    print(f"Waiting for stack {stack_name} to reach CREATE_IN_PROGRESS, CREATE_COMPLETE, or UPDATE_COMPLETE status...")
+    attempts = 0
+    while attempts < max_attempts:
+        response = call_describe_stack_events()
+        if not response:
+            print("No response received.")
+            break
+
+        events = response['StackEvents']
+        num_events = len(events)
+
+        for i in range(0, num_events, 10):
+            batch_events = events[i:i + 10]
+            create_in_progress, update_complete, create_complete = check_stack_status(batch_events)
+
+            if create_in_progress:
+                print("Stack is in CREATE_IN_PROGRESS status.")
+            if update_complete:
+                print(f"Stack update complete for stack {stack_name}.")
+                return
+            if create_complete:
+                print(f"Stack creation complete for stack {stack_name}.")
+                return
+
+        attempts += 1
+        print(f"Attempt {attempts}/{max_attempts}: Waiting for stack to reach desired status...")
+        time.sleep(2)
+
+    print("Max attempts reached or desired status not found within the attempts limit.")
+
+
 def send_message_to_queue(message_attributes=None):
     print(f"Queue ARN is: {queue_name}")
     print(f"Message body is: {message_body}")
@@ -99,6 +159,7 @@ def setup():
 
 
 if __name__ == "__main__":
+    wait_for_stack_status(stack_name)
     send_message_to_queue()
     setup()
     check_activity_log_created()

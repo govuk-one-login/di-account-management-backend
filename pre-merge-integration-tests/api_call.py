ff77f0cba1f05c76082651f2a89b715ecf3351a7
Peter Fajemisin <peter.fajemisin@digital.cabinet-office.gov.uk>
2024-11-20 07:34:30 +0000

    Cleanup.

--- pre-merge-integration-tests/api_call.py
+++ pre-merge-integration-tests/api_call.py
@@ -1,24 +1,23 @@
 import time
-import sys
 import boto3
+import os
 from botocore.exceptions import ClientError
 
-# Initialize a session using CloudFormation
 client = boto3.client('cloudformation')
-# Create SQS client
 sqs = boto3.client('sqs')
-# Initialize the DynamoDB client
 dynamodb = boto3.resource('dynamodb')
 
-
-# Name of the CloudFormation stack
 stack_name = 'home-backend'
-message_body = "{ \"event_name\" : \"AUTH_AUTH_CODE_ISSUED\", \"event_id\" : \"75093b9c-728d-4c7f-aad2-7e5892a30be0\", \"user\" : { \"user_id\" : \"user_id\", \"session_id\" : \"7340477f-74da-46d4-9400-d22ae518da3a\" }, \"client_id\" : \"vehicleOperatorLicense\" , \"timestamp\" : \"1730800548523\" }"
+message_body = ("{ \"event_name\" : \"AUTH_AUTH_CODE_ISSUED\", \"event_id\" : "
+                "\"75093b9c-728d-4c7f-aad2-7e5892a30be0\", \"user\" : { \"user_id\" : \"user_id\", \"session_id\" : "
+                "\"7340477f-74da-46d4-9400-d22ae518da3a\" }, \"client_id\" : \"vehicleOperatorLicense\" , "
+                "\"timestamp\" : \"1730800548523\" }")
+table_name = 'activity_log'
+queue_name = os.environ('SQS_QUEUE_ARN')
 
-def send_message_to_queue(queue_url, message_body, message_attributes=None):
 
+def send_message_to_queue(queue_url, message_attributes=None):
     print(f"Message body is: {message_body}")
-
     try:
         # Send message to SQS queue
         response = sqs.send_message(
@@ -32,66 +31,9 @@ def send_message_to_queue(queue_url, message_body, message_attributes=None):
         print("Error sending message:", str(e))
 
 
-def call_describe_stack_events(stack_name):
-    print("Describing call stack event")
-    try:
-        response = client.describe_stack_events(StackName=stack_name)
-        return response
-    except Exception as e:
-        print(f"Error fetching stack events: {e}")
-        return None
-
-def check_stack_status(events):
-    print("Checking stack status")
-    create_in_progress = False
-    update_complete = False
-    create_complete = False
-
-    for event in events:
-        resource_status = event['ResourceStatus']
-        if resource_status == 'CREATE_IN_PROGRESS':
-            create_in_progress = True
-        if resource_status == 'UPDATE_COMPLETE':
-            update_complete = True
-        if resource_status == 'CREATE_COMPLETE':
-            create_complete = True
-    
-    return create_in_progress, update_complete, create_complete
-
-def wait_for_stack_status(stack_name, max_attempts=10):
-    print(f"Waiting for stack {stack_name} to reach CREATE_IN_PROGRESS, CREATE_COMPLETE, or UPDATE_COMPLETE status...")
-    attempts = 0
-    while attempts < max_attempts:
-        response = call_describe_stack_events(stack_name)
-        if not response:
-            print("No response received.")
-            break
-        
-        events = response['StackEvents']
-        num_events = len(events)
-
-        for i in range(0, num_events, 10):
-            batch_events = events[i:i + 10]
-            create_in_progress, update_complete, create_complete = check_stack_status(batch_events)
-
-            if create_in_progress:
-                print("Stack is in CREATE_IN_PROGRESS status.")
-            if update_complete:
-                print(f"Stack update complete for stack {stack_name}.")
-                return
-            if create_complete:
-                print(f"Stack creation complete for stack {stack_name}.")
-                return
-
-        attempts += 1
-        print(f"Attempt {attempts}/{max_attempts}: Waiting for stack to reach desired status...")
-        time.sleep(2)
-
-    print("Max attempts reached or desired status not found within the attempts limit.")
-
 def check_activity_log_created(event_id, user_id):
     delay = 1
-    retries=10
+    retries = 10
 
     for attempt in range(1, retries + 1):
         print(f"Attempt {attempt}...")
@@ -108,12 +50,11 @@ def check_activity_log_created(event_id, user_id):
 
 def call_get_activity_log(event_id, user_id):
     print(f"Attempting to get activity log for event_id {event_id} and user_id {user_id}")
-    table_name = 'activity_log'
     table = dynamodb.Table(table_name)
     try:
         response = table.query(
             KeyConditionExpression=boto3.dynamodb.conditions.Key('user_id').eq(user_id) &
-            boto3.dynamodb.conditions.Key('event_id').eq(event_id)
+                                   boto3.dynamodb.conditions.Key('event_id').eq(event_id)
         )
         # Get the items
         items = response.get('Items', [])
@@ -126,19 +67,18 @@ def call_get_activity_log(event_id, user_id):
         print(f"Error querying activity log: {e.response['Error']['Message']}")
 
 
-def main(args):
-    if len(args) < 2:
-        print("Usage: api_call.py <param1>")
-        return
+def teardown():
+    return
 
-    queue_url = args[1]
 
-    print(f"Queue URL is: {queue_url}")
-    send_message_to_queue(queue_url, message_body)
+def setup():
+    return
+
 
 if __name__ == "__main__":
-    main(sys.argv)
-    # setup
+    send_message_to_queue(queue_name)
+    setup()
     check_activity_log_created('75093b9c-728d-4c7f-aad2-7e5892a30be1', 'user_id')
     print("Script execution completed.")
+    teardown()
     # teardown

3963ef8196402c02b617a654f087c856ab8f443c
Saral Kaushik <saral.kaushik@dsit.gov.uk>
2026-06-03 15:53:51 +0100

    OLH-4200 add testss

--- src/tests/update-inactive-account-tracker.test.ts
+++ src/tests/update-inactive-account-tracker.test.ts
@@ -2,6 +2,7 @@ import { vi, describe, test, expect, afterEach } from "vitest";
 import { Context, DynamoDBStreamEvent } from "aws-lambda";
 import { Logger } from "@aws-lambda-powertools/logger";
 import { handler } from "../update-inactive-account-tracker.js";
+import { generateDynamoSteamRecord } from "./testFixtures.js";
 
 describe("UpdateInactiveAccountTracker handler", () => {
   const loggerInfoMock = vi
@@ -13,7 +14,7 @@ describe("UpdateInactiveAccountTracker handler", () => {
   });
 
   test("logs invocation message", async () => {
-    const event: DynamoDBStreamEvent = { Records: [] };
+    const event: DynamoDBStreamEvent = { Records: [generateDynamoSteamRecord('test-client')] };
     await handler(event, {} as Context);
     expect(loggerInfoMock).toHaveBeenCalledWith(
       "UpdateInactiveAccountTracker invoked"

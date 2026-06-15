75f15a16e4d5fcf643074648812e0df84562026c
Saral Kaushik <saral.kaushik@dsit.gov.uk>
2026-06-15 11:47:22 +0100

    OLH-4304 create user notification deletion lambda

---
+++ src/tests/delete-user-notifications.test.ts
@@ -0,0 +1,94 @@
+import { vi, describe, test, expect, beforeEach, afterEach } from "vitest";
+import { DynamoDBDocumentClient, DeleteCommand } from "@aws-sdk/lib-dynamodb";
+import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
+import { mockClient } from "aws-sdk-client-mock";
+import {
+  handler,
+  validateUserData,
+  deleteUserData,
+} from "../delete-user-notifications.js";
+
+import {
+  TEST_SNS_EVENT_WITH_TWO_RECORDS,
+  TEST_USER_DATA,
+} from "./testFixtures.js";
+import { Context } from "aws-lambda";
+
+const dynamoMock = mockClient(DynamoDBDocumentClient);
+const sqsMock = mockClient(SQSClient);
+
+describe("deleteUserData", () => {
+  beforeEach(() => {
+    dynamoMock.reset();
+    process.env.TABLE_NAME = "TABLE_NAME";
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  test("deletes item from DynamoDB", async () => {
+    await deleteUserData(TEST_USER_DATA);
+    expect(dynamoMock).toHaveReceivedCommandWith(DeleteCommand, {
+      TableName: process.env.TABLE_NAME,
+      Key: { internalCommonSubjectId: TEST_USER_DATA.user_id },
+    });
+  });
+});
+
+describe("handler", () => {
+  beforeEach(() => {
+    dynamoMock.reset();
+    sqsMock.reset();
+    process.env.TABLE_NAME = "TABLE_NAME";
+    process.env.AWS_REGION = "AWS_REGION";
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  test("it iterates over each record in the batch", async () => {
+    await handler(TEST_SNS_EVENT_WITH_TWO_RECORDS, {} as Context);
+    expect(dynamoMock.commandCalls(DeleteCommand).length).toEqual(2);
+  });
+
+  describe("error handling", () => {
+    beforeEach(() => {
+      sqsMock.reset();
+      sqsMock.on(SendMessageCommand).resolves({ MessageId: "MessageId" });
+      dynamoMock.rejectsOnce("mock error");
+    });
+
+    test("logs the error message", async () => {
+      let errorMessage;
+      try {
+        await handler(TEST_SNS_EVENT_WITH_TWO_RECORDS, {} as Context);
+      } catch (error) {
+        errorMessage = (error as Error).message;
+      }
+      expect(errorMessage).toContain(
+        "Unable to delete user notifications for message with ID: MessageId, mock error"
+      );
+    });
+  });
+});
+
+describe("validateUserData", () => {
+  test("doesn't throw an error with valid data", () => {
+    expect(validateUserData(TEST_USER_DATA)).toBe(TEST_USER_DATA);
+  });
+
+  describe("throws an error", () => {
+    test("when user_id is missing", () => {
+      const userData = JSON.parse(
+        JSON.stringify({
+          foo: "bar",
+        })
+      );
+      expect(() => {
+        validateUserData(userData);
+      }).toThrow();
+    });
+  });
+});

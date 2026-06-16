50b35dbe737a58a1fbac9de9743fe96766c9371c
Saral Kaushik <748653+saralk@users.noreply.github.com>
2026-06-16 10:22:23 +0100

    OLH-4304 create user notification deletion lambda (#1055)
    ## Proposed changes
    
    <!-- Provide a summary of your changes in the title above -->
    <!-- Include the Jira ticket number in square brackets as prefix, eg
    `[OLH-XXXX]: PR Title` -->
    
    ### What changed
    
    - Create a lambda that deletes data from the user notifications table
    - Subscribe that lambda to the UserDeletion SNS topic
    
    ### Why did it change
    
    <!-- Describe the reason these changes were made - the "why" -->
    
    ### Related links
    
    <!-- List any related PRs -->
    <!-- List any related ADRs or RFCs -->
    
    ## Checklists
    
    <!-- Merging this PR deploys to production. Please answer accurately.
    -->
    
    ### Environment variables or secrets
    
    - [ ] No environment variables or secrets were added or changed
    
    <!-- Delete if changes DO NOT include new environment variables or
    secrets -->
    
    - [ ] Added parameters to Cloudformation template
    - [ ] Added new secret or systems manager parameter
    - [ ] Added new environment variable
    
    ### Permissions
    
    - [ ] This PR adds or changes permissions
    
    ### Monitoring
    
    - [ ] This PR includes changes that need to be monitored in production
    as the scope of the change could cause issues
    
    ## Testing
    
    <!-- Provide a summary of any manual testing you've done, for example
    deploying the branch to dev -->
    
    ## How to review
    
    <!-- Describe any non-standard steps to review this work, or highlight
    any areas that you'd like the reviewer's opinion on -->

---
+++ src/delete-user-notifications.ts
@@ -0,0 +1,71 @@
+import { Context, SNSEvent } from "aws-lambda";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  DeleteCommand,
+  DeleteCommandOutput,
+} from "@aws-sdk/lib-dynamodb";
+import { UserData } from "./common/model.js";
+import { getEnvironmentVariable } from "./common/utils.js";
+import { Logger } from "@aws-lambda-powertools/logger";
+
+const logger = new Logger();
+
+const marshallOptions = {
+  convertClassInstanceToMap: true,
+};
+const translateConfig = { marshallOptions };
+
+const dynamoClient = new DynamoDBClient({});
+const dynamoDocClient = DynamoDBDocumentClient.from(
+  dynamoClient,
+  translateConfig
+);
+
+export const validateUserData = (userData: UserData): UserData => {
+  if (userData.user_id) {
+    return userData;
+  }
+  throw new Error(
+    `userData did not have a user_id: ${JSON.stringify(userData)}`
+  );
+};
+
+export const deleteUserData = async (
+  userData: UserData
+): Promise<DeleteCommandOutput> => {
+  const TABLE_NAME = getEnvironmentVariable("TABLE_NAME");
+  const command = new DeleteCommand({
+    TableName: TABLE_NAME,
+    Key: { internalCommonSubjectId: userData.user_id },
+  });
+  return dynamoDocClient.send(command);
+};
+
+export const handler = async (
+  event: SNSEvent,
+  context: Context
+): Promise<void> => {
+  logger.addContext(context);
+  await Promise.all(
+    event.Records.map(async (record) => {
+      try {
+        logger.info(
+          `started processing message with ID: ${record.Sns.MessageId}`
+        );
+        const userData: UserData = JSON.parse(record.Sns.Message);
+        validateUserData(userData);
+        await deleteUserData(userData);
+        logger.info(
+          `finished processing message with ID: ${record.Sns.MessageId}`
+        );
+      } catch (error) {
+        throw new Error(
+          `Unable to delete user notifications for message with ID: ${record.Sns.MessageId}, ${
+            (error as Error).message
+          }`, { cause: error }
+        );
+      }
+    })
+  );
+};

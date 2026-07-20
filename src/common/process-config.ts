8c9bd6baa813bf51b20f760846839884cd753d95
Saral Kaushik <saral.kaushik@dsit.gov.uk>
2026-07-20 09:41:23 +0100

    OLH-4205 get the process config

--- src/common/process-config.ts
+++ src/common/process-config.ts
@@ -1,9 +1,28 @@
 import type { InactiveAccountStatus } from "./model.js";
 
-export type ProcessConfig = Record<string, { queueUrlEnvVar: string; daysToDeletion: number[]; allowedStatuses: InactiveAccountStatus[] }>;
+export type ProcessConfig = Record<string, {
+  queueUrlEnvVar: string;
+  daysToDeletion: number[];
+  allowedStatuses: InactiveAccountStatus[],
+  emailTemplateEnvVar?: string
+}>;
 
 export const processConfig: ProcessConfig = {
-  Warning30Day: { queueUrlEnvVar: "WARNING_30_DAY_NOTIFICATION_QUEUE_URL", daysToDeletion: [30], allowedStatuses: ["pending"] },
-  Warning7Day: { queueUrlEnvVar: "WARNING_7_DAY_NOTIFICATION_QUEUE_URL", daysToDeletion: [7], allowedStatuses: ["pending", "30DayWarningSent"] },
-  DeleteAccount: { queueUrlEnvVar: "ACCOUNT_DELETION_QUEUE_URL", daysToDeletion: [0], allowedStatuses: ["pending", "30DayWarningSent", "7DayWarningSent"] },
+  Warning30Day: {
+    queueUrlEnvVar: "WARNING_30_DAY_NOTIFICATION_QUEUE_URL",
+    daysToDeletion: [30],
+    allowedStatuses: ["pending"],
+    emailTemplateEnvVar: "WARNING_30_DAY_EMAIL_TEMPLATE_ID"
+  },
+  Warning7Day: {
+    queueUrlEnvVar: "WARNING_7_DAY_NOTIFICATION_QUEUE_URL",
+    daysToDeletion: [7],
+    allowedStatuses: ["pending", "30DayWarningSent"],
+    emailTemplateEnvVar: "WARNING_7_DAY_EMAIL_TEMPLATE_ID"
+  },
+  DeleteAccount: {
+    queueUrlEnvVar: "ACCOUNT_DELETION_QUEUE_URL",
+    daysToDeletion: [0],
+    allowedStatuses: ["pending", "30DayWarningSent", "7DayWarningSent"]
+  },
 };
--- src/send-inactive-warning-email.ts
+++ src/send-inactive-warning-email.ts
@@ -1,6 +1,8 @@
 import { Context, SQSEvent } from "aws-lambda";
 import { Logger } from "@aws-lambda-powertools/logger";
 import { initMetrics } from "./common/metrics.js";
+import { processConfig } from "./common/process-config.js";
+import assert from "node:assert/strict"
 
 const logger = new Logger();
 const metrics = initMetrics("send-inactive-warning-email");
@@ -11,12 +13,23 @@ export const handler = async (
 ): Promise<void> => {
   logger.addContext(context);
 
+
   for (const record of event.Records) {
     const body = JSON.parse(record.body);
+    const process = processConfig[body.processName];
+
     logger.info("Processing inactive account warning", {
       commonSubjectId: body.commonSubjectId,
       processName: body.processName,
     });
+
+    assert(process, `Process configuration not found for ${body.processName}`);
+
+    if (!process.allowedStatuses.includes(body.status)) {
+      logger.info(`Status ${body.status} is not allowed for process ${body.processName}`);
+      return;
+    }
+
   }
 
   metrics.publishStoredMetrics();

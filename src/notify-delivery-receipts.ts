cc1778b3e6f77a888fc8e28aa6d6cd55339526be
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2026-08-06 16:51:36 +0100

    OLH-4554: add delivery receipts handler

---
+++ src/notify-delivery-receipts.ts
@@ -0,0 +1,41 @@
+import { getSecret } from "@aws-lambda-powertools/parameters/secrets";
+import { normalizeAPIGatewayProxyEventHandlerWrapper } from "./common/normalizeAPIGatewayProxyEventHandlerWrapper/index.js";
+import { APIGatewayProxyHandler } from "./common/types.js";
+import { Logger } from "@aws-lambda-powertools/logger";
+import assert from "node:assert";
+
+const logger = new Logger();
+
+export const handler: APIGatewayProxyHandler =
+  normalizeAPIGatewayProxyEventHandlerWrapper(async (event) => {
+    if (!event.headers["authorization"]) {
+      logger.info("Authorization header not set");
+
+      return {
+        statusCode: 403,
+        body: "TODO1 remove body value",
+      };
+    }
+
+    assert.ok(
+      process.env["NOTIFY_DELIVERY_RECEIPTS_SECRET_ARN"],
+      "NOTIFY_DELIVERY_RECEIPTS_SECRET_ARN not set"
+    );
+
+    if (
+      event.headers["authorization"].split("Bearer ")[1] !==
+      (await getSecret(process.env["NOTIFY_DELIVERY_RECEIPTS_SECRET_ARN"]))
+    ) {
+      logger.info("Invalid Authorization header value");
+
+      return {
+        statusCode: 403,
+        body: "TODO2 remove body value",
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: "TODO3 remove body value",
+    };
+  });
